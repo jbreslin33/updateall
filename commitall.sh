d708d1003664df2d9e47703963f728b0a6b17a9b
jbreslin33 <jbreslin33@localhost>
2010-10-18 17:45:52 -0400

    add files

---
+++ commitall.sh
@@ -0,0 +1,26 @@
+cd ..
+cd baseapplication
+hg commit -u jbreslin33
+hg push
+cd ..
+cd breslininput
+hg commit -u jbreslin33
+hg push
+cd ..
+cd breslinmathracer
+hg commit -u jbreslin33
+hg push
+cd ..
+cd charactercontrollers
+hg commit -u jbreslin33
+hg push
+cd ..
+cd questionfactory
+hg commit -u jbreslin33
+hg push
+cd ..
+cd updateall
+hg commit -u jbreslin33
+hg push
+cd ..
+
