498f263b6b54846db3f20ca0c5c1b40568b5c894
jbreslin33 <jbreslin33@localhost>
2010-11-10 08:29:50 -0500

    ...

--- cloneall.sh
+++ cloneall.sh
@@ -1,12 +1,12 @@
 cd ..
 hg clone https://baseapplication.googlecode.com/hg/ baseapplication 
-hg clone https://baseapplication.googlecode.com/hg/ breslininput
-hg clone https://baseapplication.googlecode.com/hg/ breslinmathracer 
-hg clone https://baseapplication.googlecode.com/hg/ charactercontrollers
-hg clone https://baseapplication.googlecode.com/hg/ mathproblems
-hg clone https://baseapplication.googlecode.com/hg/ ogretutorials 
-hg clone https://baseapplication.googlecode.com/hg/ ogretuts 
-hg clone https://baseapplication.googlecode.com/hg/ questionfactory 
-hg clone https://baseapplication.googlecode.com/hg/ updateall 
-hg clone https://baseapplication.googlecode.com/hg/ visomathtxtgame 
+hg clone https://breslininput.googlecode.com/hg/ breslininput
+hg clone https://breslinmathracer.googlecode.com/hg/ breslinmathracer 
+hg clone https://charactercontrollers.googlecode.com/hg/ charactercontrollers
+hg clone https://mathproblems.googlecode.com/hg/ mathproblems
+hg clone https://ogretutorials.googlecode.com/hg/ ogretutorials 
+hg clone https://ogretuts.googlecode.com/hg/ ogretuts 
+hg clone https://questionfactory.googlecode.com/hg/ questionfactory 
+hg clone https://updateall.googlecode.com/hg/ updateall 
+hg clone https://visomathtxtgame.googlecode.com/hg/ visomathtxtgame 
 cd updateall
