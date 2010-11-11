4836e3c3cac7f65f56d92d661f91487a2b61f336
jbreslin33 <jbreslin33@localhost>
2010-11-11 05:07:49 -0500

    ..

--- cloneall.sh
+++ cloneall.sh
@@ -5,8 +5,6 @@ hg clone https://breslinmathracer.googlecode.com/hg/ breslinmathracer
 hg clone https://breslinnetwork.googlecode.com/hg/ breslinnetwork 
 hg clone https://charactercontrollers.googlecode.com/hg/ charactercontrollers
 hg clone https://mathproblems.googlecode.com/hg/ mathproblems
-hg clone https://ogretutorials.googlecode.com/hg/ ogretutorials 
-hg clone https://ogretuts.googlecode.com/hg/ ogretuts 
 hg clone https://questionfactory.googlecode.com/hg/ questionfactory 
 hg clone https://updateall.googlecode.com/hg/ updateall 
 hg clone https://visomathtxtgame.googlecode.com/hg/ visomathtxtgame 
