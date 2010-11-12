f68838fd50bcaf5a8436f938e60cbddb3fe6bcd7
jbreslin33 <jbreslin33@localhost>
2010-11-12 15:00:47 -0500

    ...

--- commitall.sh
+++ commitall.sh
@@ -2,34 +2,62 @@ cd ..
 cd baseapplication
 hg commit -u jbreslin33
 hg push
-cd ..
-cd breslininput
-hg commit -u jbreslin33
-hg push
-cd ..
-cd breslinmathracer
-hg commit -u jbreslin33
-hg push
-cd ..
-cd breslinnetwork
-hg commit -u jbreslin33
-hg push
+
 cd ..
 cd breslinclient
 hg commit -u jbreslin33
 hg push
+
 cd ..
-cd charactercontrollers
+cd breslingameserver
 hg commit -u jbreslin33
 hg push
+
 cd ..
-cd questionfactory
+cd breslininput
 hg commit -u jbreslin33
 hg push
+
 cd ..
 cd breslinlistenserver
 hg commit -u jbreslin33
 hg push
+
+cd ..
+cd breslinmathracer
+hg commit -u jbreslin33
+hg push
+
+cd ..
+cd breslinmathracer3dserver
+hg commit -u jbreslin33
+hg push
+
+cd ..
+cd breslinmathracertxtserver
+hg commit -u jbreslin33
+hg push
+
+cd ..
+cd breslinnetwork
+hg commit -u jbreslin33
+hg push
+
+cd ..
+cd breslinserver
+hg commit -u jbreslin33
+hg push
+
+cd ..
+cd charactercontrollers
+hg commit -u jbreslin33
+hg push
+
+cd ..
+cd questionfactory
+hg commit -u jbreslin33
+hg push
+
 cd ..
 cd updateall
 hg commit -u jbreslin33
--- updateall.sh
+++ updateall.sh
@@ -2,34 +2,67 @@ cd ..
 cd baseapplication
 hg pull
 hg update
-cd ..
-cd breslininput
-hg pull
-hg update
-cd ..
-cd breslinmathracer
-hg pull
-hg update
-cd ..
-cd breslinnetwork
-hg pull
-hg update
+
 cd ..
 cd breslinclient
 hg pull
 hg update
+
 cd ..
-cd charactercontrollers
+cd breslingameserver
 hg pull
 hg update
+
 cd ..
-cd questionfactory
+cd breslininput
 hg pull
 hg update
+
 cd ..
 cd breslinlistenserver
 hg pull
 hg update
+
+cd ..
+cd breslinmathracer
+hg pull
+hg update
+
+cd ..
+cd breslinmathracer3dserver
+hg pull
+hg update
+
+cd ..
+cd breslinmathtxtserver
+hg pull
+hg update
+
+cd ..
+cd breslinnetwork
+hg pull
+hg update
+
+cd ..
+cd breslinserver
+hg pull
+hg update
+
+cd ..
+cd charactercontrollers
+hg pull
+hg update
+
+cd ..
+cd questionfactory
+hg pull
+hg update
+
+cd ..
+cd breslinlistenserver
+hg pull
+hg update
+
 cd ..
 cd updateall
 hg pull
