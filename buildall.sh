26748b7dfdefc57bad32c1e2f88d1ff4edf40178
jbreslin33 <jbreslin33@localhost>
2010-11-14 19:36:26 -0500

    ..

---
+++ buildall.sh
@@ -0,0 +1,57 @@
+cd /home/jbreslin/sandbox/ 
+mkdir build
+cd build
+
+cd ..
+mkdir breslinclient
+cd breslinclient
+cmake ../../breslinclient
+make
+
+mkdir breslinlistenserver
+cd breslinlistenserver
+cmake ../../breslinlistenserver
+make
+
+cd ..
+mkdir breslingameserver
+cd breslingameserver
+cmake ../../breslingameserver
+make
+
+cd ..
+mkdir breslintalker
+cd breslintalker
+cmake ../../breslintalker
+make
+
+cd ..
+mkdir breslinmathracer
+cd breslinmathracer
+cmake ../../breslinmathracer
+make
+
+cd ..
+mkdir breslinmessagehandler
+cd breslinmessagehandler
+cmake ../../breslinmessagehandler
+make
+
+cd ..
+mkdir breslinquestiongameclient
+cd breslinquestiongameclient
+cmake ../../breslinquestiongameclient
+make
+
+cd ..
+mkdir breslinquestiongameserver
+cd breslinquestiongameserver
+cmake ../../breslinquestiongameserver
+make
+
+#go back to update all
+cd ..
+cd ..
+cd updateall
+
+
