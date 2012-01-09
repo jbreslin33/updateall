d4ac5f087c8481318ec21bfe39dade829587d4b3
jbreslin33 <jbreslin33@localhost>
2012-01-09 18:19:21 -0500

    fixed formating

--- math/counting/count.php
+++ math/counting/count.php
@@ -1,5 +1,4 @@
 <?php
-
         // Start up your PHP Session
         session_start();
 
@@ -7,9 +6,7 @@
         if ($_SESSION["Login"] != "YES") {
           header("Location: login/form.php");
         }
-
-        ?>
-
+?>
 
 <html>
 <head>
@@ -21,58 +18,52 @@ var answer = 0;
 
 function submitAnswer(a)
 {
-  if (a == 1)
-  {
-    answer = document.getElementById("button1").innerHTML;
-  }
-  else if (a == 2)
-  {
-    answer = document.getElementById("button2").innerHTML;
-  }
+	if (a == 1)
+  	{
+    		answer = document.getElementById("button1").innerHTML;
+  	}
+  	else if (a == 2)
+  	{
+    		answer = document.getElementById("button2").innerHTML;
+  	}
 
-  else if (a == 3)
-  {
-    answer = document.getElementById("button3").innerHTML;
-  }
-  else if (a == 4)
-  {
-    answer = document.getElementById("button4").innerHTML;
-  }
-  checkAnswer();
+  	else if (a == 3)
+  	{
+    		answer = document.getElementById("button3").innerHTML;
+  	}
+  	else if (a == 4)
+  	{
+    		answer = document.getElementById("button4").innerHTML;
+  	}
+ 	checkAnswer();
 }
-
 
 function checkAnswer()
 {
-  if (answer == correctAnswer)
-  {
- 
-     document.getElementById("feedback").innerHTML="correct";
-     count++;
-     correctAnswer++;
-var offset = Math.floor(Math.random() *2);
-offset = count - offset;
+  	if (answer == correctAnswer)
+  	{
+     		document.getElementById("feedback").innerHTML="correct";
+     		count++;
+     		correctAnswer++;
+		var offset = Math.floor(Math.random() *2);
+		offset = count - offset;
      
-	document.getElementById("counter").innerHTML=count;
-     document.getElementById("button1").innerHTML=offset;
-     document.getElementById("button2").innerHTML=offset + 1;
-     document.getElementById("button3").innerHTML=offset + 2;
-     document.getElementById("button4").innerHTML=offset + 3;
-  }
-  else
-  {
-     
-
-document.getElementById("feedback").innerHTML="wrong";
-	count = 0;
-	correctAnswer = 1;
-  
-     
-	document.getElementById("counter").innerHTML=count;
-     document.getElementById("button1").innerHTML=count;
-     document.getElementById("button2").innerHTML=count + 1;
-     document.getElementById("button3").innerHTML=count + 2;
-     document.getElementById("button4").innerHTML=count + 3;
+		document.getElementById("counter").innerHTML=count;
+     		document.getElementById("button1").innerHTML=offset;
+     		document.getElementById("button2").innerHTML=offset + 1;
+     		document.getElementById("button3").innerHTML=offset + 2;
+     		document.getElementById("button4").innerHTML=offset + 3;
+  	}
+  	else
+  	{
+		document.getElementById("feedback").innerHTML="wrong";
+		count = 0;
+		correctAnswer = 1;
+		document.getElementById("counter").innerHTML=count;
+     		document.getElementById("button1").innerHTML=count;
+     		document.getElementById("button2").innerHTML=count + 1;
+     		document.getElementById("button3").innerHTML=count + 2;
+     		document.getElementById("button4").innerHTML=count + 3;
 	}
 }
 </script>
