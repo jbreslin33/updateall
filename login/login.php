9fff65fb8d5ce9a03952e26c885f160bd05ea7c6
jbreslin33 <jbreslin33@localhost>
2012-01-09 19:47:44 -0500

    works. hopefully it now stores the id in cookie

--- login/login.php
+++ login/login.php
@@ -8,6 +8,10 @@
 	
 	<?php
 
+	//user id
+	$id = 0;
+
+	//db connection
  	$db = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibesfat");
 
 	//query string 	
@@ -33,17 +37,40 @@
 	//get numer of rows
    	$num = pg_num_rows($dbResult);
 
+	//get user id and store it in session var
+	
+	$i = 0;
+	while ($i < $num)
+	{	
+		$id = pg_Result ($dbResult, $i, 'id');
+		$i++;
+	}
+
 	// if there is a row then the username and password pair exists
 	if ($num > 0)
 	{
+		//start new session	
 		session_start();
+		
+		//set login var to yes	
 	  	$_SESSION["Login"] = "YES";
-          	header("Location: ../math/counting/count.php");
+          
+		//set user id to be used later			
+		$_SESSION["id"] = $id;  	
+		
+		//send to game 1 for now but i need to interject here and send
+		//user to his game_id		
+		header("Location: ../math/counting/count.php");
 	}
 	else
 	{
-	  	session_start();
-	  	$_SESSION ["Login"] = "NO";
+	  	//start new session	
+		session_start();
+	  	
+		//set login cookie to no	
+		$_SESSION ["Login"] = "NO";
+		
+		//send user to login as we don't care what level they are at until they login succesfully
 		header("Location: login_form.php");
 	}
 
