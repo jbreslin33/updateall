e79c1f574deca98f42f244a9a97095330c51303f
jbreslin33 <jbreslin33@localhost>
2012-01-12 13:58:49 -0500

    this should give yoiu enought to get started i now hav urls

--- game.php
+++ game.php
@@ -15,6 +15,8 @@ $conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibes
 
 $id = $_SESSION["id"];
 
+//------math game level-----------------------------------------------
+
 //query string
 $query = "select math_game_level ";
 $query .= "from users ";
@@ -25,7 +27,7 @@ $query .= ";";
 
 echo "<h1> Query: $query </h1>";
 
-//get db restult
+//get db result
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
 //get numer of rows
@@ -33,14 +35,45 @@ $num = pg_num_rows($result);
 
 echo "<h1>  Number of Rows: $num </h1>";
 
+$math_game_level = 0;
+
 // if there is a row then id exists it better be unique!
 if ($num > 0)
 {
-	//$math_game_level = pg_result_seek($result, 0);
-	$row = pg_fetch_row($result);	
-	echo "<h1> math_game_level: $row[0] </h1>";
+	$row = pg_fetch_row($result);
+	$math_game_level = $row[0];	
 }
 
+echo "<h1> math_game_level: $math_game_level </h1>";
+
+//--------------------------url----------------------
+echo "<h1> URL Section </h1>";
+//sql query 
+//query string
+$query = "select url ";
+$query .= "from math_games ";
+$query .= "where level = ";
+$query .= $math_game_level;
+$query .= ";";
+
+//get db restult for url
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//get numer of rows 
+$num = pg_num_rows($result);
+
+echo "<h1>  Number of Rows: $num </h1>";
+
+$url = "";
+
+// if there is a row then id exists it better be unique!
+if ($num > 0)
+{
+	$row = pg_fetch_row($result);
+	$url = $row[0];
+}
+
+echo "<h1> url: $url </h1>";
 ?>
 
 
