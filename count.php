4f8de2efee5deda3296b0116089b77d385c00288
jbreslin33 <jbreslin33@localhost>
2012-01-12 20:16:06 -0500

    works. now to grab the values using row[]
    r

--- count.php
+++ count.php
@@ -12,8 +12,6 @@ if ($_SESSION["Login"] != "YES")
 }
 
 
-echo "<h1> hello count </h1>";
-
 $math_game_level = $_SESSION["math_game_level"];
 
 //db connection
@@ -25,7 +23,7 @@ $conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibes
 or die('Could not connect: ' . pg_last_error());
 
 //query
-$query = "select * ";
+$query = "select name, start_number, end_number ";
 $query .= "from math_games ";
 $query .= "where level = ";
 $query .= $_SESSION["math_game_level"];
@@ -34,7 +32,20 @@ $query .= ";";
 
 echo "<h1> Query: $query </h1>";
 
-echo "<h1> hello count </h1>";
+//get db result
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//get numer of rows
+$num = pg_num_rows($result);
+
+echo "<h1>  Number of Rows: $num </h1>";
+
+
+//get user id and store it in session var
+//$name = pg_Result ($dbResult, 0, 'name');
+//$start_number = pg_Result ($dbResult, 0, 'start_number');
+//$end_number = pg_Result ($dbResult, 0, 'end_number');
+
 
 ?>
 
