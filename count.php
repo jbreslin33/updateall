348911053d7f4bfe91b23c485e5c9d5ad21a5768
jbreslin33[A <jbreslin33[A@localhost>
2012-01-13 17:37:28 -0500

    added check_login.php to call before each file.

--- count.php
+++ count.php
@@ -1,16 +1,9 @@
 <html>
 <body>
 
+<?php include("check_login.php"); ?>
+
 <?php
-
-session_start();
-
-// If the user is not logged in send him/her to the login form
-if ($_SESSION["Login"] != "YES")
-{
-        header("Location: login_form.php");
-}
-
 
 $math_game_level = $_SESSION["math_game_level"];
 
@@ -30,7 +23,7 @@ $query .= $_SESSION["math_game_level"];
 $query .= ";";
 
 
-echo "<h1> Query: $query </h1>";
+//echo "<h1> Query: $query </h1>";
 
 //get db result
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
@@ -38,7 +31,7 @@ $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error())
 //get numer of rows
 $num = pg_num_rows($result);
 
-echo "<h1>  Number of Rows: $num </h1>";
+//echo "<h1>  Number of Rows: $num </h1>";
 
 $name = "";
 $start_number = 0;
@@ -53,9 +46,9 @@ if ($num > 0)
 	$end_number = $row[2];
 }
 
-echo "<h1> name: $name </h1>";
-echo "<h1> start_number: $start_number </h1>";
-echo "<h1> end_number: $end_number </h1>"; 
+//echo "<h1> name: $name </h1>";
+//echo "<h1> start_number: $start_number </h1>";
+//echo "<h1> end_number: $end_number </h1>"; 
 
 
 echo "<script type=\"text/javascript\">";
