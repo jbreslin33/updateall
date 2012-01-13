066073df854e44d0574afea04e684de803c275d6
jbreslin33 <jbreslin33@localhost>
2012-01-12 19:13:15 -0500

    check for login works.

--- count.php
+++ count.php
@@ -1,5 +1,19 @@
 <html>
 <body>
+
+<?php
+
+session_start();
+
+// If the user is not logged in send him/her to the login form
+if ($_SESSION["Login"] != "YES")
+{
+        header("Location: login_form.php");
+}
+
+?>
+
 <h1> hello count </h1>
+
 </body>
 </html> 
