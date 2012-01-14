7542734f1a0941a592cf4e04051ade03a40e6b33
jbreslin33 <jbreslin33@localhost>
2012-01-13 21:08:42 -0500

    u

--- goto_next_math_level.php
+++ goto_next_math_level.php
@@ -7,25 +7,21 @@ $conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibes
 
 //------math game level-----------------------------------------------
 
+//update the level +100
 $math_game_level = $_SESSION["math_game_level"];
 $math_game_level = $math_game_level + 100;
 
-//query string
-//$query = "select math_game_level ";
-//$query .= "from users ";
-//$query .= "where id = ";
-//$query .= $_SESSION["id"];
-//$query .= ";";
-//update users set math_game_level = 200 where id = 3;
+//query 
 $query = "update users set math_game_level = ";
 $query .= $math_game_level;
 $query .= " where id = ";
 $query .= $_SESSION["id"];
 $query .= ";"; 
 
-//get db result
+//db call to update
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
+//send player to the game page where he will be redirected.
 header("Location: game.php");
 
 ?>
