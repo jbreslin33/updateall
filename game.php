998b9696d31a8c2a4def3b71d1dbcddd6a77d89d
jbreslin33 <jbreslin33@localhost>
2012-01-14 21:56:48 -0500

    took out debug echo's

--- game.php
+++ game.php
@@ -16,15 +16,11 @@ $query = "select math_game_level from users where id = ";
 $query .= $_SESSION["id"];
 $query .= ";";
 
-echo "<h1> Query: $query </h1>";
-
 //get db result
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
 //get numer of rows
 $num = pg_num_rows($result);
-
-echo "<h1>  Number of Rows: $num </h1>";
 
 // if there is a row then id exists it better be unique!
 if ($num > 0)
@@ -33,13 +29,7 @@ if ($num > 0)
 	$_SESSION["math_game_level"] = $row[0];	
 }
 
-echo "<h1> math_game_level:";
-echo $_SESSION["math_game_level"];
-echo "</h1>";
-
 //--------------------------url----------------------
-echo "<h1> URL Section </h1>";
-//sql query 
 //query string
 $query = "select url from math_games where level = ";
 $query .= $_SESSION["math_game_level"];
@@ -50,16 +40,12 @@ $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error())
 
 //get numer of rows 
 $num = pg_num_rows($result);
-echo "<h1>  Number of Rows: $num </h1>";
 
 // if there is a row then id exists it better be unique!
 if ($num > 0)
 {
 	$row = pg_fetch_row($result);
 	$_SESSION["url"] = $row[0];
-	echo "<h1> url: ";
-	echo $_SESSION["url"];
-	echo "</h1>";
 
 	//header("Location: $_SESSION[\"url\"]");
 	header("Location: $row[0]");
