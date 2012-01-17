d3bd8b7fbc935d600f7e20d7c5a50c208e43b65a
jbreslin33 <jbreslin33@localhost>
2012-01-17 14:20:38 -0500

    adding signup.php

--- sign_up_form.php
+++ sign_up_form.php
@@ -1,17 +1,30 @@
 <html>
-	<head>
+
+<head>
 	<title>Login</title>
 
-	</head>
-	<body>
+</head>
+
+<body>
 	<form method="post" action="login.php">
 
 	<p>Username: <input type="text" name="username" /></p>
 	<p>Password: <input type="text" name="password" /></p>
 
-	<p><input type="submit" value="Let me in" /></p>
+	<p><input type="submit" value="Log In" /></p>
 
 	</form>
-	</body>
-	</html>
+
+	<form method="post" action="sign_up.php">
+
+	<p>Username: <input type="text" name="username" /></p>
+	<p>Password: <input type="text" name="password" /></p>
+
+	<p><input type="submit" value="Sign Up" /></p>
+
+	</form>
+	
+</body>
+
+</html>
 
