424a0e11aa278ba43672d1343c5856a845470aa7
jbreslin33 <jbreslin33@localhost>
2012-01-21 17:56:18 -0500

    adding game_chooser.php so i can use that instead of game.php. i want to use game.php for game class.

---
+++ game_chooser.php
@@ -0,0 +1,59 @@
+<?php include("check_login.php"); ?>
+<?php include("db_connect.php"); ?>
+
+<?php
+
+//db connection
+$conn = dbConnect();
+
+//------math game level-----------------------------------------------
+
+//query string
+$query = "select math_game_level from users where id = ";
+$query .= $_SESSION["id"];
+$query .= ";";
+
+//get db result
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//get numer of rows
+$num = pg_num_rows($result);
+
+// if there is a row then id exists it better be unique!
+if ($num > 0)
+{
+	$row = pg_fetch_row($result);
+	$_SESSION["math_game_level"] = $row[0];	
+}
+else
+{
+	header("Location: login_form.php");
+}
+
+//--------------------------url----------------------
+
+//query string
+$query = "select url from math_games where level = ";
+$query .= $_SESSION["math_game_level"];
+$query .= ";";
+
+//get db restult for url
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//get numer of rows 
+$num = pg_num_rows($result);
+
+// if there is a row then id exists it better be unique!
+if ($num > 0)
+{
+	$row = pg_fetch_row($result);
+	$_SESSION["url"] = $row[0];
+
+	header("Location: $row[0]");
+}
+else
+{
+	header("Location: login_form.php");
+}
+
+?>
