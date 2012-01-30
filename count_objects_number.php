53560915a0298fc888c91cac763efb2fd232fc1e
jbreslin33 <jbreslin33@localhost>
2012-01-30 11:24:52 -0500

    u

--- count_objects_number.php
+++ count_objects_number.php
@@ -1,73 +1,15 @@
 <?php
 
-include("top.php"); 
-
-//query
-$query = "select name, start_number, score_needed, count_by, number_of_buttons from math_games where level = ";
-$query .= $_SESSION["math_game_level"];
-$query .= ";";
-
-//get db result
-$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
-
-//game variables to fill from db
-$name = "";
-$startNumber = 0;
-$scoreNeeded = 0; 
-$countBy = 0;
-$numberOfButtons = 0;
-
-//get numer of rows
-$num = pg_num_rows($result);
-
-// if there is a row then id exists it better be unique!
-if ($num > 0)
-{
-	//get row
-        $row = pg_fetch_row($result);
-       	
-	//fill php vars from db 
-	$name = $row[0];
-	$startNumber = $row[1];
-	$scoreNeeded = $row[2];
-	$countBy = $row[3];
-	$numberOfButtons = $row[4];
-}
+include("top.php");
+include("middle.php");
 
 ?>
 
-<!-- inheritance class -->
-<script type="text/javascript" src="kinherit.js"></script>
-
-<!-- base game class, this should be extended -->
-<?php include("game.php"); ?>
-
-<!-- sub class of game for counting -->
-<?php include("game_count_write.php"); ?>
-
-<!-- sub class -->
+<!-- class for game -->
 <?php include("game_count_objects_number.php"); ?>
 
-<!-- creating gameCount which is child of game -->
-<script type="text/javascript">  var game = new GameCountObjectNumber( <?php echo "$startNumber,$scoreNeeded,$countBy,$numberOfButtons);"; ?> </script>
+<!-- creating game --> 
+<script type="text/javascript">  var game = new Game( <?php echo "$startNumber,$scoreNeeded,$countBy,$numberOfButtons);"; ?> </script>
 
-<!-- creat and set game name -->
-<h1 = id="game_name"> <?php echo "$name"; ?> </h1>
-
-<!-- create and set question -->
-<p id="question"> </p>
-
-<!-- Create Buttons (could this be done in db?) --> 
-<?php 
-
-$i=1;
-for ($i=1; $i < $numberOfButtons + 1; $i++)
-{
-	echo "<button type=\"button\" id=\"button$i\" onclick=\"game.submitGuess(this.id)\"> </button> ";
-}
-
-?>
-
-<!-- lower.php --> 
+<!-- lower.php -->
 <?php include("lower.php"); ?>
-
--- game_count_objects_number.php
+++ game_count_objects_number.php
@@ -1,42 +1,172 @@
 <script type="text/javascript">
 
-function GameCountObjectNumber(startNumber,scoreNeeded,countBy,numberOfButtons)
+function Game(startNumber,scoreNeeded,countBy,numberOfButtons)
 {
-	GameCountObjectNumber.baseConstructor.call(this,startNumber,scoreNeeded,countBy,numberOfButtons);
+	//score
+        this.score=0;
+        this.scoreNeeded = scoreNeeded;
+
+        //game
+        this.numberOfButtons = numberOfButtons;
+        this.question="";
+        this.guess=0;
+        this.answer=0;
+
+        //count
+        this.countBy = countBy;
+        this.count=0;
+        this.startNumber = startNumber;
+
+	//Write
+	this.answers = new Array();
+	this.answers[0] = "0";
+	this.answers[1] = "1";
+	this.answers[2] = "2";
+	this.answers[3] = "3";
+	this.answers[4] = "4";
+	this.answers[5] = "5";
+	this.answers[6] = "6";
+	this.answers[7] = "7";
+	this.answers[8] = "8";
+	this.answers[9] = "9";
+	this.answers[10] = "1";
+	this.answers[11] = "0";
+	this.answers[12] = "1";
+	this.answers[13] = "1";
+	this.answers[14] = "1";
+	this.answers[15] = "2";
+	this.answers[16] = "1";
+	this.answers[17] = "3";
+	this.answers[18] = "1";
+	this.answers[19] = "4";
+	this.answers[20] = "1";
+	this.answers[21] = "5";
+	this.answers[22] = "1";
+	this.answers[23] = "6";
+	this.answers[24] = "1";
+	this.answers[25] = "7";
+	this.answers[26] = "1";
+	this.answers[27] = "8";
+	this.answers[28] = "1";
+	this.answers[29] = "9";
+	this.answers[30] = "2";
+	this.answers[31] = "0";
+
 }
 
-// subclass 
-KInherit.extend(GameCountObjectNumber, GameCountWrite);
-
-//over rides
-GameCountObjectNumber.prototype.newQuestion = function()
+//Score
+Game.prototype.printScore = function()
 {
-        //set question
-	this.question = Math.floor(Math.random() *10);
-	this.question++;
-	
-	this.removeImages();
-	this.createImages();	
+        document.getElementById("score").innerHTML="Score: " + this.score;
+        document.getElementById("scoreNeeded").innerHTML="Score Needed: " + this.scoreNeeded;
 }
 
-//new functions
-GameCountObjectNumber.prototype.createImages = function()
+Game.prototype.checkForEndOfGame = function()
 {
-	var i = 0;	
-	for (i=0; i < this.question; i++)
+        if (this.score == <?php echo "$scoreNeeded"; ?> )
+        {
+                document.getElementById("feedback").innerHTML="YOU WIN!!!";
+                window.location = "goto_next_math_level.php"
+        }
+}
+
+//reset
+Game.prototype.resetVariables = function()
+{
+        //score
+        this.score = 0;
+
+        //game
+        this.question = "";
+        this.guess = 0;
+        this.answer = 0;
+
+        //count
+        this.count = this.startNumber;
+}
+
+Game.prototype.checkGuess = function()
+{
+        if (this.guess == this.answer)
+        {
+                this.count = this.count + this.countBy;  //add to count
+                this.score++;
+
+                document.getElementById("feedback").innerHTML="Correct!";
+
+                this.checkForEndOfGame();
+        }
+        else
+        {
+                document.getElementById("feedback").innerHTML="Wrong! Try again.";
+
+                this.resetVariables();
+        }
+}
+
+
+//overide
+Game.prototype.newQuestion = function()
+{
+        
+	//set question
+
+        if (this.count < 10 || this.count == 10 || this.count == 12 || this.count == 14 || this.count == 16 || this.count == 18 || this.count == 20
+         || this.count == 22 || this.count == 24 || this.count == 26 || this.count == 28 || this.count == 30)
+        {
+                this.question = this.question + ' ' +  this.answers[this.count];
+        }
+        else
+        {
+                this.question = this.question + '' +  this.answers[this.count];
+        }
+        document.getElementById("question").innerHTML=this.question;
+}
+
+Game.prototype.setChoices = function()
+{
+ 	//set buttons
+        var offset = Math.floor(Math.random() *4);
+        offset = this.answer - offset;
+        this.setButtons(0);
+}
+
+Game.prototype.newAnswer = function()
+{
+   this.answer = this.answers[this.count + this.countBy];
+
+}
+
+Game.prototype.setButtons = function(offset)
+{
+	i=1;
+	for (i=1; i < this.numberOfButtons + 1; i++)
 	{
-		var img = new Image();   // Create new img element  
-		img.src = 'redball.gif'; // Set source path	
-		document.getElementById("question").appendChild(img);	
+		j = i - 1;
+		document.getElementById("button" + i).innerHTML=offset + j;
 	}
 }
 
-
-GameCountObjectNumber.prototype.removeImages = function()
+Game.prototype.submitGuess = function(button_id)
 {
-	while (document.getElementById("question").hasChildNodes()) 	
-	{
-		document.getElementById("question").removeChild(document.getElementById("question").firstChild);	
-	}
+	this.guess = document.getElementById(button_id).innerHTML;
+        this.checkGuess();
+        this.printScore();
+        this.newQuestion();
+        this.newAnswer();
+        this.setChoices();
 }
+
+Game.prototype.init = function()
+{
+        this.resetVariables();
+        
+        this.newQuestion();
+        this.newAnswer();
+        this.setChoices();
+        this.printScore();
+}
+
+
+
 </script>
