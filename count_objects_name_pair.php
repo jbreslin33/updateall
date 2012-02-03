c621280597d06888b04269e1a8fb898c9aa44c0b
jbreslin33 <jbreslin33@localhost>
2012-02-03 14:21:40 -0500

    uu

--- count_objects_name_pair.php
+++ count_objects_name_pair.php
@@ -1,6 +1,8 @@
 <?php
 
 include("top.php");
+
+
 include("middle.php");
 
 ?>
@@ -10,6 +12,32 @@ include("middle.php");
 
 <!-- creating game --> 
 <script type="text/javascript">  var game = new Game( <?php echo "$startNumber,$scoreNeeded,$countBy,$numberOfButtons);"; ?> </script>
+
+<style>
+	#draggable { width: 150px; height: 150px; padding: 0.5em; }
+	</style>
+	<script type="text/javascript">
+	$(function() {
+		$( "#draggable" ).draggable();
+	});
+	</script>
+
+
+
+<div class="demo">
+
+<div id="draggable" class="ui-widget-content">
+	<p>Drag me around</p>
+</div>
+
+</div><!-- End demo -->
+
+
+
+<div style="display: none;" class="demo-description">
+<p>Enable draggable functionality on any DOM element. Move the draggable object by clicking on it with the mouse and dragging it anywhere within the viewport.</p>
+</div><!-- End demo-description -->
+
 
 <!-- lower.php -->
 <?php include("lower.php"); ?>
