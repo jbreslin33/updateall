0cbd7ac6331bc9df7cbb3a3ddc4e0b9b20895b87
jbreslin33 <jbreslin33@localhost>
2012-02-23 11:30:52 -0500

    u

--- webserver.js
+++ webserver.js
@@ -2,10 +2,10 @@ var http = require('http');
 
 http.createServer(function (request, response) {
 //  response.writeHead(200, {'Content-Type': 'text/plain'});
-    setTimeout(function(){
         response.writeHead(200, {'Content-Type': 'text/plain'});
+    setTimeout(function(){
       response.end('End of the World\n');
-    }, 20000);
+    }, 2000);
 
 
 //  response.end('Hello World\n');
