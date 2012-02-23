7fb6dccfe895b0e76003899dbb6536e0f2438b00
jbreslin33 <jbreslin33@localhost>
2012-02-23 13:26:32 -0500

    u

--- webserver.js
+++ webserver.js
@@ -2,10 +2,34 @@ var http = require('http');
 
 http.createServer(function (request, response) {
 //  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+var buffer = [];
+io.on('connection', function(client){
+    client.send({ buffer: buffer });
+    client.broadcast({ announcement: client.sessionId + ' connected' });
+
+    client.on('message', function(message){
+        var msg = { message: [client.sessionId, message] };
+        buffer.push(msg);
+        if (buffer.length > 15) buffer.shift();
+        client.broadcast(msg);
+    });
+
+    client.on('disconnect', function(){
+        client.broadcast({ announcement: client.sessionId + ' disconnected' });
+    });
+});
+
+
         response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write('Its');
     setTimeout(function(){
       response.end('End of the World\n');
-    }, 2000);
+    }, 20000);
+
+
+
+
 
 
 //  response.end('Hello World\n');
