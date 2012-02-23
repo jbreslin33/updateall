259b656b13445e5e65eeca7ff645a8d15e7606ed
jbreslin33 <jbreslin33@localhost>
2012-02-23 11:22:57 -0500

    u

--- webserver.js
+++ webserver.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+//  response.writeHead(200, {'Content-Type': 'text/plain'});
+    setTimeout(function(){
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end('End of the World\n');
+    }, 20000);
+
+
+//  response.end('Hello World\n');
+
+
 }).listen(30004);
 
 console.log('Server running at http://127.0.0.1:30004/');
