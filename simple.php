0df05d90009855561da705da406c134e741fb658
jbreslin33 <jbreslin33@localhost>
2012-02-24 20:34:50 -0500

    u

--- simple.php
+++ simple.php
@@ -7,23 +7,22 @@ DIV.movable { position:absolute; }
 <script language="javascript">
 var x = 5; //Starting Location - left
 var y = 5; //Starting Location - top
-var dest_x = 300;  //Ending Location - left
-var dest_y = 300;  //Ending Location - top
 var interval = 1; //Move 10px every initialization
 
 function moveBot(bot) {
 	//Keep on moving the image till the target is achieved
-	if(x<dest_x) x = x + interval; 
-	if(y<dest_y) y = y + interval;
+	x = x + interval; 
+	y = y + interval;
 	
 	//Move the image to the new location
 	bot.style.top  = y+'px';
 	bot.style.left = x+'px';
 
 }
+
 function update()
 {
-	moveBot(document.getElementById("ufo"));
+	moveBot(document.getElementById("avatar_id"));
 	
 	window.setTimeout('update()',16);
 
@@ -31,7 +30,7 @@ function update()
 </script>
 </head>
 <body onload="update()">
-<div id="ufo" class="movable">
+<div id="avatar_id" class="movable">
 <img src="redball.gif" alt="Please link to a vaild image." />
 </div>
 
--- test.php
+++ test.php
@@ -11,7 +11,7 @@ var dest_x = 300;  //Ending Location - left
 var dest_y = 300;  //Ending Location - top
 var interval = 1; //Move 10px every initialization
 
-function moveImage() {
+function update() {
 	//Keep on moving the image till the target is achieved
 	if(x<dest_x) x = x + interval; 
 	if(y<dest_y) y = y + interval;
@@ -23,12 +23,12 @@ function moveImage() {
 	if ((x+interval < dest_x) && (y+interval < dest_y)) {
 		//Keep on calling this function every 100 microsecond 
 		//	till the target location is reached
-		window.setTimeout('moveImage()',16);
+		window.setTimeout('update()',16);
 	}
 }
 </script>
 </head>
-<body onload="moveImage()">
+<body onload="update()">
 <div id="ufo" class="movable">
 <img src="redball.gif" alt="Please link to a vaild image." />
 </div>
