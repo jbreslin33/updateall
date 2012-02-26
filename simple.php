bde5fe3b944b0b3311e79bd57fe7520bcde9aa5c
jbreslin33 <jbreslin33@localhost>
2012-02-26 12:17:56 -0500

    u

--- simple.php
+++ simple.php
@@ -8,8 +8,54 @@
 <script type="text/javascript" src="jquery-1.7.js"></script>
 <script type="text/javascript" src="jquery-ui-1.8.17.custom.min.js"></script>
 
+<?php 
+include("check_login.php");
+include("db_connect.php");
+
+//db connection
+$conn = dbConnect();
+
+//query
+$query = "select name, score_needed, count_by, start_number, end_number, tick_length from math_games where level = ";
+$query .= $_SESSION["math_game_level"];
+$query .= ";";
+
+//get db result
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//game variables to fill from db
+$name = "";
+$scoreNeeded = 0;
+$countBy = 0;
+$startNumber = 0;
+$endNumber = 0;
+$tickLength = 0;
+
+//get numer of rows
+$num = pg_num_rows($result);
+
+// if there is a row then id exists it better be unique!
+if ($num > 0)
+{
+        //get row
+        $row = pg_fetch_row($result);
+
+        //fill php vars from db
+        $name = $row[0];
+        $scoreNeeded = $row[1];
+        $countBy = $row[2];
+        $startNumber = $row[3];
+        $endNumber = $row[4];
+        $tickLength = $row[5];
+}
+
+?>
+
+
 
 <script language="javascript">
+
+
 
 var x = 5; //Starting Location - left
 var y = 5; //Starting Location - top
