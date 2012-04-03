e71f1cc91d6ded94ffbf3310d47f8a21d8fad568
jbreslin33 <jbreslin33@localhost>
2012-04-03 12:16:57 -0400

    u8

--- basic.php
+++ basic.php
@@ -228,56 +228,56 @@ var Shape = new Class(
 
 	ai: function()
 	{
-			if (this.mAI == true)
-			{
-				var direction = Math.floor(Math.random()*9)	
-	                	if (direction == 0) //left
-                        	{
-                                	this.mKeyX = -1;
-                                	this.mKeyY = 0;
-                        	}
-                        	if (direction == 1) //right
-                        	{
-                                	this.mKeyX = 1;
-                                	this.mKeyY = 0;
-                        	}
-                        	if (direction == 2) //up
-                        	{
-                                	this.mKeyX = 0;
-                                	this.mKeyY = -1;
-                        	}
-                        	if (direction == 3) //down
-                        	{
-                                	this.mKeyX = 0;
-                                	this.mKeyY = 1;
-                        	}
-                        	if (direction == 4) //leftup
-                        	{
-                                	this.mKeyX = -.5;
-                                	this.mKeyY = -.5;
-                        	}
-                        	if (direction == 5) //leftdown
-                        	{
-                                	this.mKeyX = -.5;
-                                	this.mKeyY = .5;
-                        	}
-                        	if (direction == 6) //rightup
-                        	{
-                                	this.mKeyX = .5;
-                                	this.mKeyY = -.5;
-                        	}
-                        	if (direction == 7) //rightdown
-                        	{
-                                	this.mKeyX = .5;
-                                	this.mKeyY = .5;
-                        	}
-                        	if (direction == 8) //stop
-                        	{
-                                	this.mKeyX = 0;
-                                	this.mKeyY = 0;
-                        	}
-			} 
-		},
+		if (this.mAI == true)
+		{
+			var direction = Math.floor(Math.random()*9)	
+                	if (direction == 0) //left
+                       	{
+                               	this.mKeyX = -1;
+                               	this.mKeyY = 0;
+                       	}
+                       	if (direction == 1) //right
+                       	{
+                               	this.mKeyX = 1;
+                               	this.mKeyY = 0;
+                       	}
+                       	if (direction == 2) //up
+                       	{
+                               	this.mKeyX = 0;
+                               	this.mKeyY = -1;
+                       	}
+                       	if (direction == 3) //down
+                       	{
+                               	this.mKeyX = 0;
+                               	this.mKeyY = 1;
+                       	}
+                       	if (direction == 4) //leftup
+                       	{
+                               	this.mKeyX = -.5;
+                               	this.mKeyY = -.5;
+                       	}
+                       	if (direction == 5) //leftdown
+                       	{
+                               	this.mKeyX = -.5;
+                               	this.mKeyY = .5;
+                       	}
+                       	if (direction == 6) //rightup
+                       	{
+                               	this.mKeyX = .5;
+                               	this.mKeyY = -.5;
+                       	}
+                       	if (direction == 7) //rightdown
+                       	{
+                               	this.mKeyX = .5;
+                              	this.mKeyY = .5;
+                       	}
+                       	if (direction == 8) //stop
+                       	{
+                               	this.mKeyX = 0;
+                               	this.mKeyY = 0;
+                       	}
+		} 
+	},
 
 	draw: function()
 	{
