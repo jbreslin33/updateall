9af6d426c11d1b19e797497e18f938333a753964
jbreslin33 <jbreslin33@localhost>
2012-04-12 11:12:30 -0400

    u application does nothing.

--- src/application/application.php
+++ src/application/application.php
@@ -8,98 +8,14 @@ var Application = new Class(
 {
         initialize: function(tickLength)
         {
-                //window size
-                this.mWindow = window.getSize();
-
-                //ticks
-                this.mTickLength = tickLength;
-                
-                //time
-                this.mTimeSinceEpoch = 0;
-                this.mLastTimeSinceEpoch = 0;
-                this.mTimeSinceLastInterval = 0;
-                
-                //key pressed
-                this.mKeyLeft = false;
-                this.mKeyRight = false;
-                this.mKeyUp = false;
-                this.mKeyDown = false;
-                this.mKeyStop = false;
-
-                mGenre = new GenreAdventure(this, scoreNeeded, countBy, startNumber, endNumber, numberOfChasers, speed, leftBounds, rightBounds, topBounds, bottomBounds, collisionDistance);
-                
-                //this will be used for resetting to
-                mGenre.resetGame();
-
-        },
+        
+	},
 
         update: function()
         {
-                if (mGenre.mGenreOn)
-                {
-                        //get time since epoch and set lasttime 
-                        e = new Date();
-                        this.mLastTimeSinceEpoch = this.mTimeSinceEpoch;
-                        this.mTimeSinceEpoch = e.getTime();
-                
-                        //set timeSinceLastInterval as function of timeSinceEpoch and LastTimeSinceEpoch diff
-                        this.mTimeSinceLastInterval = this.mTimeSinceEpoch - this.mLastTimeSinceEpoch;
-                
-                        //update genre
-                        mGenre.update(); 
-                
-//                        var t=setTimeout("mApplication.update()",20)
-                }
-        },
-
-        log: function(msg)
-        {
-                setTimeout(function()
-                {
-                        throw new Error(msg);
-                }, 0);
-        },
-
-        //CONTROLS
-        keyDown: function(event)
-        {
-                if (event.key == 'left')
-                {
-                        mApplication.mKeyLeft = true;
-                }
-                if (event.key == 'right')
-                {
-                        mApplication.mKeyRight = true;
-                }
-                if (event.key == 'up')
-                {
-                        mApplication.mKeyUp = true;
-                }
-                if (event.key == 'down')
-                {
-                        mApplication.mKeyDown = true;
-                }
-        },
         
-        keyUp: function(event)
-        {       
-                if (event.key == 'left')
-                {
-                        mApplication.mKeyLeft = false;
-                }
-                if (event.key == 'right')
-                {
-                        mApplication.mKeyRight = false;
-                }
-                if (event.key == 'up')
-                {
-                        mApplication.mKeyUp = false;
-                }
-                if (event.key == 'down')
-                {
-                        mApplication.mKeyDown = false;
-                }
-        }
+	},
+
 });
 
 
