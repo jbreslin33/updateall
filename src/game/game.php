d6fbe2c11155cb379b7650c5f439c875a56e3a4b
jbreslin33 <jbreslin33@localhost>
2012-04-14 22:13:38 -0400

    adding dungeon game specific stuff to dungeon game and taking it out of game.

--- src/game/game.php
+++ src/game/game.php
@@ -99,14 +99,6 @@ var Game = new Class(
 
 	createWorld: function()
 	{
-                //create Shapes 
-                this.createShapes();
-
-                //create walls          
-                this.createLeftWall();
-                this.createRightWall();
-                this.createTopWall();
-                this.createBottomWall();
 		
 	},
 
@@ -434,36 +426,8 @@ var Game = new Class(
         //reset
         resetGame: function()
         {
-                //set collidable to true 
-                for (i=0; i < this.mShapeArray.length; i++)
-                {
-         
-                        //set every shape to spawn position     
-                        this.mShapeArray[i].mPositionX = this.mShapeArray[i].mSpawnPositionX;
-                        this.mShapeArray[i].mPositionY = this.mShapeArray[i].mSpawnPositionY;
-
-                        if (this.mShapeArray[i].mCollidable == true)
-                        { 
-                                this.mShapeArray[i].mCollisionOn = true;
-				this.mShapeArray[i].setVisibility(true);
-                        }
-                }
-                this.mControlObject.mPositionX = 0;     
-                this.mControlObject.mPositionY = 0;     
- 
-                //score
-                this.mQuiz.mScore = 0;
-
-                //game
-                this.mQuiz.mQuestion = this.mQuiz.mCount;
-
-                //count
-                this.mQuiz.mCount = this.mQuiz.mStartNumber;
         
-                //answer
-                this.mQuiz.newAnswer();
-		this.mShapeArray[0].setText(this.mQuiz.mCount);
-        }
+	}
 });
 
 
--- src/game/game_dungeon_count.php
+++ src/game/game_dungeon_count.php
@@ -15,7 +15,54 @@ Extends: Game,
         update: function()
         {
                 this.parent();
-        }
+        },
+	
+	createWorld: function()
+	{
+                //create Shapes
+                this.createShapes();
+
+                //create walls
+                this.createLeftWall();
+                this.createRightWall();
+                this.createTopWall();
+                this.createBottomWall();
+
+	},
+
+	resetGame: function()
+	{
+              //set collidable to true
+                for (i=0; i < this.mShapeArray.length; i++)
+                {
+
+                        //set every shape to spawn position
+                        this.mShapeArray[i].mPositionX = this.mShapeArray[i].mSpawnPositionX;
+                        this.mShapeArray[i].mPositionY = this.mShapeArray[i].mSpawnPositionY;
+
+                        if (this.mShapeArray[i].mCollidable == true)
+                        {
+                                this.mShapeArray[i].mCollisionOn = true;
+                                this.mShapeArray[i].setVisibility(true);
+                        }
+                }
+                this.mControlObject.mPositionX = 0;
+                this.mControlObject.mPositionY = 0;
+
+                //score
+                this.mQuiz.mScore = 0;
+
+                //game
+                this.mQuiz.mQuestion = this.mQuiz.mCount;
+
+                //count
+                this.mQuiz.mCount = this.mQuiz.mStartNumber;
+
+                //answer
+                this.mQuiz.newAnswer();
+                this.mShapeArray[0].setText(this.mQuiz.mCount);
+
+	}
 
 });
 
