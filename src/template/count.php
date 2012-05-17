bf79287c7ccd5abfa85bbbb97ba5975ea1c5a923
jbreslin33 <jbreslin33@localhost>
2012-05-16 23:12:32 -0400

    made a header.php file in template that should handle basic level db calls and html. this way count.php and subsequent files like it should be more game specific.

--- src/template/count.php
+++ src/template/count.php
@@ -1,45 +1,6 @@
-<html>
-<head>
-
-<title>ABC AND YOU</title>
-
-<!-- mootools -->
-<script type="text/javascript" src="../mootools/mootools-core-1.4.5-full-compat.js"></script>
-
 <?php 
-include("../login/check_login.php");
-include("../database/db_connect.php");
-
-//db connection
-$conn = dbConnect();
-
-//query the level table
-$query = "select skill, next_level from levels where level = ";
-$query .= $_SESSION["math_game_level"];
-$query .= ";";
-
-//get db result
-$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
-
-//game variables to fill from db
-$skill = "";
-$nextLevel = 0;
-
-//get numer of rows
-$num = pg_num_rows($result);
-
-// if there is a row then id exists it better be unique!
-if ($num > 0)
-{
-        //get row
-        $row = pg_fetch_row($result);
-
-        //fill php vars from db
-        $skill = $row[0];
-	$nextLevel = $row[1];
-	
-	$_SESSION["math_game_next_level"] = $nextLevel;
-}
+//standard header for most games i hope. it handles some basic html and level db call
+include("../template/header.php");
 
 //query the game table, eventually maybe there will be more than one result here which would be a choice of game for that level.
 $query = "select score_needed, count_by, start_number, end_number from math_games where level = ";
---
+++ src/template/header.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+
+<title>ABC AND YOU</title>
+
+<!-- mootools -->
+<script type="text/javascript" src="../mootools/mootools-core-1.4.5-full-compat.js"></script>
+
+<?php 
+include("../login/check_login.php");
+include("../database/db_connect.php");
+
+//db connection
+$conn = dbConnect();
+
+//query the level table
+$query = "select skill, next_level from levels where level = ";
+$query .= $_SESSION["math_game_level"];
+$query .= ";";
+
+//get db result
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//game variables to fill from db
+$skill = "";
+$nextLevel = 0;
+
+//get numer of rows
+$num = pg_num_rows($result);
+
+// if there is a row then id exists it better be unique!
+if ($num > 0)
+{
+        //get row
+        $row = pg_fetch_row($result);
+
+        //fill php vars from db
+        $skill = $row[0];
+	$nextLevel = $row[1];
+	
+	$_SESSION["math_game_next_level"] = $nextLevel;
+}
+?>
