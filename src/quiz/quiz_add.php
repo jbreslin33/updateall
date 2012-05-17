dd7c6c4411299aebd5dd43cffaf8c1fa5a9965a2
jbreslin33 <jbreslin33@localhost>
2012-05-17 15:43:08 -0400

    making add game

--- src/quiz/quiz_add.php
+++ src/quiz/quiz_add.php
@@ -3,17 +3,20 @@ public methods
 ----------------
 
 //get methods
-count       getCount(); returns current answer
-countBy     getCountBy(); //returns current score
-startNumber getStartNumber();
-endNumber   getEndNumber();
 
 //set methods
-void setCount(count);
-void setCountBy(countBy);
-void setStartNumber(startNumber);
-void setEndNumber(endNumber);
-
+  //questions
+                this.mQuestionArray.push(new Question("4 + 3",7));
+                this.mQuestionArray.push(new Question("4 + 2",6));
+                this.mQuestionArray.push(new Question("4 + 1",5));
+                this.mQuestionArray.push(new Question("2 + 2",4));
+                this.mQuestionArray.push(new Question("3 + 3",6));
+                this.mQuestionArray.push(new Question("1 + 1",2));
+                this.mQuestionArray.push(new Question("2 + 3",5));
+                this.mQuestionArray.push(new Question("4 + 4",8));
+                this.mQuestionArray.push(new Question("2 + 1",3));
+                this.mQuestionArray.push(new Question("0 + 3",3));
+ Math.floor( Math.random()*xSize) 
 ****************************************/
 
 var QuizAdd = new Class(
@@ -21,27 +24,64 @@ var QuizAdd = new Class(
 
 Extends: Quiz,
         
-	initialize: function(scoreNeeded,startNumber,endNumber)
+	initialize: function(scoreNeeded,addendMin,addendMax,numberOfAddends,game)
         {
 		//parent
 		this.parent(scoreNeeded);
 
-		//countBy
-                this.mStartNumber = startNumber;
-                this.mEndNumber = endNumber;
+		this.mGame = game;
 
-		//questions
-		this.mQuestionArray.push(new Question("4 + 3",7));
-		this.mQuestionArray.push(new Question("4 + 2",6));
-		this.mQuestionArray.push(new Question("4 + 1",5));
-		this.mQuestionArray.push(new Question("2 + 2",4));
-		this.mQuestionArray.push(new Question("3 + 3",6));
-		this.mQuestionArray.push(new Question("1 + 1",2));
-		this.mQuestionArray.push(new Question("2 + 3",5));
-		this.mQuestionArray.push(new Question("4 + 4",8));
-		this.mQuestionArray.push(new Question("2 + 1",3));
-		this.mQuestionArray.push(new Question("0 + 3",3));
-        }
+		//countBy
+		this.mAddendMin = addendMin;
+		this.mAddendMax = addendMax;
+                this.mNumberOfAddends = numberOfAddends;
+
+		for (i = 0; i <= this.mScoreNeeded; i++)
+		{
+			seed = addendMax - addendMin;
+			result1 = Math.floor(Math.random()*seed);
+			result2 = Math.floor(Math.random()*seed);
+			addend1 = result1 + addendMin;
+			addend2 = result2 + addendMin;
+			answer = addend1 + addend2;
+			this.mQuestionArray.push(new Question(addend1 + ' + ' + addend2, answer));
+		}
+		
+		count = 0;
+                for (i = 0; i <= this.mScoreNeeded; i++)
+                {
+                        var openPoint = this.mGame.getOpenPoint2D(-400,400,-300,300,50,4);
+                        this.mGame.addToShapeArray(new ShapeQuestion("",50,50,openPoint.mX,openPoint.mY,i,"yellow","","question",this.mGame,this.getSpecificQuestion(count)));
+                        count++;
+                }
+	
+
+	
+		/******************** HUD ********************/
+                this.mQuestionHud    = new Shape("",140,50,0,250,"Question:","violet","","hud");
+
+        },
+	
+	createQuiz: function()
+	{
+
+	},
+
+	reset: function()
+	{
+		this.parent();	
+                
+	  	//update hud
+                this.mQuestionHud.setText("Question: " + this.mQuestionArray[this.mMarker].getQuestion());
+	
+	},
+
+	correctAnswer: function()
+	{
+		this.parent();
+		this.mQuestionHud.setText("Question: " + this.mQuestionArray[this.mMarker].getQuestion());
+	}
+
 });
 
 
