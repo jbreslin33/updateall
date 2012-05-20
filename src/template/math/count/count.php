ba1a45a5f5a8b85c89fa90fb15e8ee9e61c16682
jbreslin33 <jbreslin33@localhost>
2012-05-20 18:37:36 -0400

    at botoom

--- src/template/math/count/count.php
+++ src/template/math/count/count.php
@@ -81,7 +81,7 @@ window.addEvent('domready', function()
 	mGame.mControlObject.showQuestionObject(false);
 
 	//numberMount to go on top let's make it small and draw it on top 
-	var numberMountee = new Shape(mGame,   "center",new Question(1,0),"",25,25,100,100,"","orange","","numberMountee");	
+	var numberMountee = new Shape(mGame,   "center",new Question(1,0),"",1,1,100,100,"","orange","","numberMountee");	
 	mGame.addToShapeArray(numberMountee); 
 	
 	//do the mount	
