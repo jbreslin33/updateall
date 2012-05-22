3924fbbde4cba28c6bea6f44b20314207b36e288
jbreslin33 <jbreslin33@localhost>
2012-05-22 13:04:07 -0400

    works for count. now just need to extrapolate out.

--- src/database/goto_next_math_level.php
+++ src/database/goto_next_math_level.php
@@ -5,14 +5,9 @@
 //db connection
 $conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibesfat") or die('Could not connect: ' . pg_last_error());
 
-//------math game level-----------------------------------------------
-
-//update the level +100
-$math_next_level = $_SESSION["math_next_level"];
-
 //query 
 $query = "update users set math_level = ";
-$query .= $math_next_level;
+$query .= $_SESSION["math_next_level"];
 $query .= " where id = ";
 $query .= $_SESSION["id"];
 $query .= ";"; 
@@ -20,10 +15,10 @@ $query .= ";";
 //db call to update
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
-$_SESSION["math_next_level"] = $math_next_level;
+$_SESSION["math_level"] = $_SESSION["math_next_level"];
 
 //send player to the game page where he will be redirected.
-header("Location: ../template/choosers/chooser.php");
+header("Location: ../template/choosers/chooser.php?table_name=math_games");
 
 ?>
 
