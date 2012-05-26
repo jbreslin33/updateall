42c20f81eaad37173c57bd8f9c597e385989e42f
jbreslin33 <jbreslin33@localhost>
2012-05-26 10:20:22 -0400

    adding advanced animation class

---
+++ src/animation/animation_advanced.php
@@ -0,0 +1,102 @@
+/**********************************************
+public methods
+----------------------
+AnimationAdvanced: Handles rotating thru a series of images at intervals. 
+
+void update(deltaTime); //update the the shape using the delta time of game update
+
+//get methods
+
+/**********************************************
+protected methods
+----------------------
+
+void animate();
+
+************************************************/
+
+var AnimationAdvanced = new Class(
+{
+Extends: Animation,
+
+        initialize: function(shape)
+        {
+		this.parent(shape);
+
+		//animation Array
+		this.mAnimation = 0;
+		this.mAnimationArray = new Array(); //at this point it looks like we would have 9 or 0-8 with 0 idle and 1 north and 8 north west.
+        },
+
+/******************** PUBLIC METHODS *************/
+
+/****** UTILITY METHODS ******************/
+
+        update: function(delta)
+        {
+		//animation--let's play a certain animation base on velocity. but for now let's do it on keystroke?
+		//scratch that. velocity should determine if run or walk animation is played.
+		//so  we will simply use direction keys
+
+		//now we have to loop thru based on timer and mAnimationArray[]
+		if (this.mShape.mKey.mX == 0 && this.mShape.mKey.mY == 0)
+		{
+			//this.mAnimation = 0;
+		}
+		//north 
+		if (this.mShape.mKey.mX == 0 && this.mShape.mKey.mY == -1)
+		{
+			this.mAnimation = 1;
+		} 
+		//north east
+		if (this.mShape.mKey.mX == .5 && this.mShape.mKey.mY == -.5)
+		{
+			this.mAnimation = 2;
+		} 
+		//east
+		if (this.mShape.mKey.mX == 1 && this.mShape.mKey.mY == 0)
+		{
+			this.mAnimation = 3;
+		} 
+		//south east
+		if (this.mShape.mKey.mX == .5 && this.mShape.mKey.mY == .5)
+		{
+			this.mAnimation = 4;
+		} 
+		//south
+		if (this.mShape.mKey.mX == 0 && this.mShape.mKey.mY == 1)
+		{
+			this.mAnimation = 5;
+		} 
+		//south west
+		if (this.mShape.mKey.mX == -.5 && this.mShape.mKey.mY == .5)
+		{
+			this.mAnimation = 6;
+		} 
+		//west
+		if (this.mShape.mKey.mX == -1 && this.mShape.mKey.mY == 0)
+		{
+			this.mAnimation = 7;
+		}
+		//north west
+		if (this.mShape.mKey.mX == -.5 && this.mShape.mKey.mY == -.5)
+		{
+			this.mAnimation = 8;
+		}
+
+		this.animate(delta);
+	},
+	
+	animate: function(delta)
+	{
+		//if that animation exists 	
+		if (this.mAnimationArray[this.mAnimation])
+		{
+			//then switch to it if we are not already there.
+			if (this.mShape.mSrc != this.mAnimationArray[this.mAnimation])
+			{
+				this.mShape.setSrc(this.mAnimationArray[this.mAnimation]);
+			}	
+		}
+	}
+});
