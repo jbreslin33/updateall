d26cb90dfa8507cb2d3489f3e716a726922cae2a
jbreslin33 <jbreslin33@localhost>
2012-05-28 21:40:52 -0400

    u

--- src/div/div.php
+++ src/div/div.php
@@ -15,11 +15,11 @@ var Div = new Class(
                 this.mDiv.style.width = this.mShape.mWidth;
                 this.mDiv.style.height = this.mShape.mHeight;
         
-                //move it
+                //move it to initial spawn spot
                 this.mDiv.style.left = this.mShape.mPosition.mX+'px';
                 this.mDiv.style.top  = this.mShape.mPosition.mY+'px';
-
-                document.body.appendChild(this.mDiv);
+                
+		document.body.appendChild(this.mDiv);
         
                 this.mDiv.style.backgroundColor = this.mShape.mBackgroundColor;
         }
