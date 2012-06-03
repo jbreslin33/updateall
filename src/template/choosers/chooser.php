5d7ec38565a2eef62c3b93e9b4f503490194ee78
jbreslin33 <jbreslin33@localhost>
2012-06-03 19:49:29 -0400

    going to look at question on subject chooser and game chooser screen

--- src/template/choosers/chooser.php
+++ src/template/choosers/chooser.php
@@ -59,7 +59,7 @@ $username = $_SESSION["username"];
 <script language="javascript">
 
 var username = "<?php echo $username; ?>";
-var gamelevel = "<?php echo $gamelevel; ?>";
+var gamelevel = "<?php echo $gameLevel; ?>";
 
 </script>
 
@@ -89,33 +89,46 @@ window.addEvent('domready', function()
         document.addEvent("keydown", mApplication.keyDown);
         document.addEvent("keyup", mApplication.keyUp);
 
-	/******************* BOUNDARY WALLS AND HUD COMBO ***********/
-        northBoundsABCANDYOU = new Shape(120, 40,  0,  0,"","","","red","boundary");
+ 	/******************* BOUNDARY WALLS AND HUD COMBO ***********/
+        var y = 35;
+        northBoundsABCANDYOU = new Shape(120, y,  0,  0,"","","","red","boundary");
         northBoundsABCANDYOU.setText('ABCANDYOU');
 
-        northBoundsUser = new Shape     (120, 40,120,  0,"","","","orange","boundary");
+        northBoundsUser = new Shape     (120, y,120,  0,"","","","orange","boundary");
         northBoundsUser.setText('User : ' + username);
 
-        northBoundsMathLevel = new Shape(120, 40,240,  0,"","","","yellow","boundary");
+        northBoundsMathLevel = new Shape(120, y,240,  0,"","","","yellow","boundary");
         northBoundsMathLevel.setText('Math Level : ' + gamelevel);
 
-        northBoundsScore = new Shape    (120, 40,360,  0,"","","","LawnGreen","boundary");
+        northBoundsScore = new Shape    (120, y,360,  0,"","","","LawnGreen","boundary");
         northBoundsScore.setText('Score : ');
 
-        northBoundsScoreNeeded = new Shape    (120, 40,480,  0,"","","","cyan","boundary");
+        northBoundsScoreNeeded = new Shape    (120, y,480,  0,"","","","cyan","boundary");
         northBoundsScoreNeeded.setText('Score Needed : ');
 
-        northBoundsGameName = new Shape (200, 40,600,  0,"","","","#DBCCE6","boundary");
-        northBoundsGameName.setText('Game : Game Chooser');
+        northBoundsGameName = new Shape (170, y,600,  0,"","","","#DBCCE6","boundary");
+        northBoundsGameName.setText('Game Chooser');
 
-        eastBounds  = new Shape         ( 10,370,790, 40,"","","","DeepPink","boundary");
+        eastBounds  = new Shape         ( 10, 50,760, 35,"","","","#F8CDF8","boundary");
+        eastBounds  = new Shape         ( 10, 50,760, 85,"","","","#F6C0F6","boundary");
+        eastBounds  = new Shape         ( 10, 50,760,135,"","","","#F5B4F5","boundary");
+        eastBounds  = new Shape         ( 10, 50,760,185,"","","","#F6C0F6","boundary");
+        eastBounds  = new Shape         ( 10, 50,760,235,"","","","#F5B4F5","boundary");
+        eastBounds  = new Shape         ( 10, 50,760,285,"","","","#F3A8F3","boundary");
+        eastBounds  = new Shape         ( 10, 50,760,335,"","","","#F19BF1","boundary");
+        eastBounds  = new Shape         ( 10, 20,760,385,"","","","#F08EF0","boundary");
 
-        southBoundsQuestion = new Shape (800, 40,  0,410,"","","","DeepPink","boundary");
+        southBoundsQuestion = new Shape (770, y,  0,405,"","","","violet","boundary");
 
+        westBounds  = new Shape         ( 10, 50,  0, 35,"","","","#F8CDF8","boundary");
+        westBounds  = new Shape         ( 10, 50,  0, 85,"","","","#F6C0F6","boundary");
+        westBounds  = new Shape         ( 10, 50,  0,135,"","","","#F5B4F5","boundary");
+        westBounds  = new Shape         ( 10, 50,  0,185,"","","","#F6C0F6","boundary");
+        westBounds  = new Shape         ( 10, 50,  0,235,"","","","#F5B4F5","boundary");
+        westBounds  = new Shape         ( 10, 50,  0,285,"","","","#F3A8F3","boundary");
+        westBounds  = new Shape         ( 10, 50,  0,335,"","","","#F19BF1","boundary");
+        westBounds  = new Shape         ( 10, 20,  0,385,"","","","#F08EF0","boundary");
 
-        westBounds  = new Shape         ( 10,370,  0, 40,"","","","DeepPink","boundary");
-
-	
 	//the game
         mGame = new GameChooser("Game Chooser");
 
