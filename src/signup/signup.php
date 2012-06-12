493d3427cd993ed73fa2c0a5cb98f8b5621f194a
jbreslin33 <jbreslin33@localhost>
2012-06-12 14:38:17 -0400

    signup now works again. It was not working due to me adding a not null field.

--- src/signup/signup.php
+++ src/signup/signup.php
@@ -1,15 +1,21 @@
 <?php include("../database/db_connect.php"); ?>
 
 <?php
+
+	//set username and password
+	$_SESSION["username"] = $_POST["username"];
+	$_SESSION["password"] = $_POST["password"];
+
 	//db connection
 	$conn = dbConnect();
 	
 	//query string 	
-	$query = "INSERT INTO users(username, password) VALUES ('";
-	$query .= $_POST["username"];
+	$query = "INSERT INTO users(username, password, role_id) VALUES ('";
+	$query .= $_SESSION["username"];
 	$query .= "', '"; 
-	$query .= $_POST["password"];
-	$query .= "');";      	
+	$query .= $_SESSION["password"];
+	$query .= "',2"; 
+	$query .= ");";      	
 
 	// insert into users......
 	$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
