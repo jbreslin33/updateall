1b65002ffe4404683ffaf87493b01d0fb37b857b
jbreslin33 <jbreslin33@localhost>
2012-06-13 13:15:35 -0400

    ready for non-number code

--- src/signup/signup.php
+++ src/signup/signup.php
@@ -12,6 +12,7 @@
 
 	$usernameString = $_SESSION["username"];
 
+	$noNumbers = true;
 
         $wordCount = str_word_count($usernameString);
 
@@ -27,110 +28,113 @@
 		}
 	}
 
+
 	else
 	{
-
-	//check for one word username	
-/*
-	$usernameArray = str_split(usernameString);
-	$arraySize = count($usernameArray);
+		//check for no numbers
+		/*
+		$usernameArray = str_split(usernameString);
+		$arraySize = count($usernameArray);
 	
-	for ($i=0; $i < $arraySize; $i++)
-	{
-		if ($usernameArray[$i] == '')
+		for ($i=0; $i < $arraySize; $i++)
 		{
-                        header("Location: ../template/signup/signup_username_no_spaces.php");
+			if ($usernameArray[$i] == '')
+			{
+                        	header("Location: ../template/signup/signup_username_no_spaces.php");
+			}
 		}
-	}
-*/
+		*/
 
-	//db connection
-	$conn = dbConnect();
+		if ($noNumbers)
+		{
+
+			//db connection
+			$conn = dbConnect();
 	
-	//query string 	
-	$query = "INSERT INTO users(username, password, role_id) VALUES ('";
-	$query .= $_SESSION["username"];
-	$query .= "', '"; 
-	$query .= $_SESSION["password"];
-	$query .= "',2"; 
-	$query .= ");";      	
+			//query string 	
+			$query = "INSERT INTO users(username, password, role_id) VALUES ('";
+			$query .= $_SESSION["username"];
+			$query .= "', '"; 
+			$query .= $_SESSION["password"];
+			$query .= "',2"; 
+			$query .= ");";      	
 
-	// insert into users......
-	$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+			// insert into users......
+			$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
 
-	//ok let's act like we are logging in.....
-	//query string  
-        $query = "select id, math_level, english_level, role_id from users where username = '";
-        $query .= $_POST["username"];
-        $query .= "' "; 
-        $query .= "and ";
-        $query .= "password = '";               
-        $query .= $_POST["password"];
-        $query .= "';";         
+			//ok let's act like we are logging in.....
+			//query string  
+        		$query = "select id, math_level, english_level, role_id from users where username = '";
+        		$query .= $_POST["username"];
+        		$query .= "' "; 
+        		$query .= "and ";
+        		$query .= "password = '";               
+        		$query .= $_POST["password"];
+        		$query .= "';";         
 
-        //get db result
-        $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+        		//get db result
+        		$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
-        //get numer of rows
-        $num = pg_num_rows($result);
+        		//get numer of rows
+        		$num = pg_num_rows($result);
         
-        //close db connection as we have the only var we needed - the id
-        pg_close();
+        		//close db connection as we have the only var we needed - the id
+        		pg_close();
        
 
-// if there is a row then the username and password pair exists
-        if ($num > 0)
-        {
-                //get the id from user table    
-                $id = pg_Result($result, 0, 'id');
-                $mathLevel = pg_Result($result, 0, 'math_level');
-                $englishLevel = pg_Result($result, 0, 'english_level');
-                $roleId = pg_Result($result, 0, 'role_id');
+			// if there is a row then the username and password pair exists
+        		if ($num > 0)
+        		{
+                		//get the id from user table    
+                		$id = pg_Result($result, 0, 'id');
+                		$mathLevel = pg_Result($result, 0, 'math_level');
+                		$englishLevel = pg_Result($result, 0, 'english_level');
+                		$roleId = pg_Result($result, 0, 'role_id');
                 
-                //set login var to yes  
-                $_SESSION["Login"] = "YES";
+                		//set login var to yes  
+                		$_SESSION["Login"] = "YES";
           
-                //set user id, and subject levels to be used later                      
-                $_SESSION["id"] = $id;          
-                $_SESSION["username"] = $_POST["username"];     
-                $_SESSION["math_level"] = $mathLevel;   
-                $_SESSION["english_level"] = $englishLevel;     
-                $_SESSION["role_id"] = $roleId;         
+                		//set user id, and subject levels to be used later                      
+                		$_SESSION["id"] = $id;          
+                		$_SESSION["username"] = $_POST["username"];     
+                		$_SESSION["math_level"] = $mathLevel;   
+                		$_SESSION["english_level"] = $englishLevel;     
+                		$_SESSION["role_id"] = $roleId;         
         
-
-                //administrator
-                if ($_SESSION["role_id"] == 1) 
-                {
+                		//administrator
+                		if ($_SESSION["role_id"] == 1) 
+                		{
         
-                }
+                		}
                 
-                //teacher
-                if ($_SESSION["role_id"] == 2) 
-                {
-                        header("Location: ../template/main/main.php");
-                }
+                		//teacher
+                		if ($_SESSION["role_id"] == 2) 
+                		{
+                        		header("Location: ../template/main/main.php");
+                		}
                 
-                //student
-                if ($_SESSION["role_id"] == 3) 
-                {
-                        header("Location: ../template/choosers/chooser_subject.php");
-                }
+                		//student
+                		if ($_SESSION["role_id"] == 3) 
+                		{
+                        		header("Location: ../template/choosers/chooser_subject.php");
+                		}
                 
-                //guest
-                if ($_SESSION["role_id"] == 4) 
-                {
+                		//guest
+                		if ($_SESSION["role_id"] == 4) 
+                		{
         
-                }
-        }
-        else
-        {
-                //set login cookie to no        
-                $_SESSION ["Login"] = "NO";
+                		}
+        		}
+        		else
+        		{
+                		//set login cookie to no        
+                		$_SESSION ["Login"] = "NO";
                 
-                //send user back to login form
-                header("Location: login_form.php");
-        }
-}
+                		//send user back to login form
+                		header("Location: login_form.php");
+        		}
+		}
+	}
 ?>
 
