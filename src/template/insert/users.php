00a7dbfd7747c85534bc0e5ee4c2c85004ea606d
jbreslin33 <jbreslin33@localhost>
2012-06-18 12:47:50 -0400

    not sure how this works but using example from here: http://w3resource.com/PostgreSQL/PostgreSQL-with-php-a-through-introduction.php

--- src/template/insert/users.php
+++ src/template/insert/users.php
@@ -12,19 +12,19 @@ include("../headers/header.php");
 <a href="../edit/edit.php">EDIT PAGE</a> 
 <a href="../select/users.php">SHOW MY STUDENTS</a> 
 
-<script language="javascript">
-window.addEvent('domready', function()
-{
 
-}
-
-);
-
-window.onresize = function(event)
-{
-
-}
-</script>
+<h2>Enter information regarding book</h2>
+<ul>
+<form name="insert" action="users.php" method="POST" >
+<li>Book ID:</li><li><input type="text" name="bookid" /></li>
+<li>Book Name:</li><li><input type="text" name="book_name" /></li>
+<li>Author:</li><li><input type="text" name="author" /></li>
+<li>Publisher:</li><li><input type="text" name="publisher" /></li>
+<li>Date of publication:</li><li><input type="text" name="dop" /></li>
+<li>Price (USD):</li><li><input type="text" name="price" /></li>
+<li><input type="submit" /></li>
+</form>
+</ul>
 
 </body>
 </html>
