f9f8ee4fde133a9cdfca73d6199be82967bd67a5
jbreslin33 <jbreslin33@localhost>
2012-06-19 13:58:13 -0400

    still need to add a home_room on add home_+room

--- src/template/insert/homeroom.php
+++ src/template/insert/homeroom.php
@@ -1,15 +1,32 @@
 <?php 
 include("../headers/header.php");
 
-$number_of_students = $_POST["number_of_students"];
+//before we loop we need a home_room
+//we first need some info, we need to know the username of admin, id of admin
+$admin_id = $_SESSION["id"];
+$admin_username = $_SESSION["username"]; 
 
+//next we need to know what user we are up to for this admin
+//$query = "select * from home_rooms where admin_id = $admin_id;";
+//$result = pg_query($query);
+//$numberOfRows = pg_num_rows($result);
+
+//add number of rows + 1 to get next number. This is based off the premise that we do not EVER delete user rows only deactivate them. 
+//$home_roomExtensionNumber = $numberOfRows + 1;
+//now let's combine admin username and userExtensionNumber to come up with a new username.
+//$newHomeRoomDescription = "HOME ROOM "; 
+//$newHomeRoomDescription .= $home_roomExtensionNumber;
+
+//$query = "insert into home_rooms (admin_id,teacher_id,description) values ('$admin_id','$admin_id','HOME ROOM');
+//$result = pg_query($query);
+
+
+
+$number_of_students = $_POST["number_of_students"];
 
 for ($i = 0; $i < $number_of_students; $i++)
 {
 
-//we first need some info, we need to know the username of admin, id of admin
-$admin_id = $_SESSION["id"];
-$admin_username = $_SESSION["username"]; 
 
 //first we need all the passwords then we can pick one at random
 $query = "select password from passwords;";
