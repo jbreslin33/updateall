aeddd219ba4cbe669b81fddd55b7d4f42e37d011
jbreslin3 <jbreslin3@localhost>
2012-06-20 17:32:21 -0400

    fixed up student select

--- src/template/select/student.php
+++ src/template/select/student.php
@@ -9,7 +9,7 @@ $admin = $_SESSION["username"];
 
 //first we need all the passwords then we can pick one at random
 $admin = $_SESSION["username"];
-$query = "select * from users where admin = '$admin' and role = 'Student';";
+$query = "select username, password, last_name, first_name, teacher, math_level, english_level from users where admin = '$admin' and role = 'Student';";
 $result = pg_query($conn,$query);
 dbErrorCheck($conn,$result);
 $numrows = pg_numrows($result);
@@ -18,9 +18,13 @@ $numrows = pg_numrows($result);
 
 <table border="1">
   <tr>
+   <th>Username</th>
+   <th>Password</th>
    <th>Last name</th>
    <th>First name</th>
-   <th>Username</th>
+   <th>Teacher</th>
+   <th>Math Level</th>
+   <th>English Level</th>
   </tr>
 
 <?
@@ -29,9 +33,13 @@ $numrows = pg_numrows($result);
    for($ri = 0; $ri < $numrows; $ri++) {
     echo "<tr>\n";
     $row = pg_fetch_array($result, $ri);
-    echo " <td>", $row["first_name"], "</td>
+    echo " <td>", $row["username"], "</td>
+   <td>", $row["password"], "</td>
    <td>", $row["last_name"], "</td>
-   <td>", $row["username"], "</td>
+   <td>", $row["first_name"], "</td>
+   <td>", $row["teacher"], "</td>
+   <td>", $row["math_level"], "</td>
+   <td>", $row["english_level"], "</td>
   </tr>
   ";
    }
