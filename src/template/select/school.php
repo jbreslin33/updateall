caaa8f8c94a5bbdd95e42e39775c1c9cb0b38727
jbreslin33 <jbreslin33@localhost>
2012-06-23 10:52:02 -0400

    adding school i don't think this is used though except by us.

---
+++ src/template/select/school.php
@@ -0,0 +1,42 @@
+<?php 
+include("../headers/header.php");
+include("../links/links.php");
+
+echo "My School";
+
+//we first need some info, we need to know the username of admin 
+$admin = $_SESSION["username"]; 
+
+//first we need all the passwords then we can pick one at random
+$admin = $_SESSION["username"];
+$query = "select homeroom, teacher from homerooms where admin = '$admin';";
+$result = pg_query($conn,$query);
+dbErrorCheck($conn,$result);
+$numrows = pg_numrows($result);
+
+?>
+
+<table border="1">
+  <tr>
+   <th>Home Room</th>
+   <th>Teacher</th>
+  </tr>
+
+<?
+   // Loop on rows in the result set.
+
+   for($ri = 0; $ri < $numrows; $ri++) {
+    echo "<tr>\n";
+    $row = pg_fetch_array($result, $ri);
+    echo " <td>", $row["homeroom"], "</td>
+   <td>", $row["teacher"], "</td>
+  </tr>
+  ";
+   }
+   pg_close($conn);
+  ?>
+  </table>
+
+</head>
+</html>
+
