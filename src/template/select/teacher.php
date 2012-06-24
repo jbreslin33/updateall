990040c2fdbb47bab55766389528ccfb83eba838
jbreslin33 <jbreslin33@localhost>
2012-06-24 15:21:20 -0400

    u

--- src/template/select/teacher.php
+++ src/template/select/teacher.php
@@ -4,7 +4,7 @@ include("../links/links.php");
 
 echo "<b><u>My Teachers:<u><b><br>";
 
-$query = "select teachers.user_id, teachers.id, users.first_name, users.last_name, teachers.room_id from teachers join users on teachers.user_id = users.id where users.school_id = ";
+$query = "select teachers.user_id, teachers.id, users.username, users.password, users.first_name, users.last_name, teachers.room_id from teachers join users on teachers.user_id = users.id where users.school_id = ";
 $query .= $_SESSION["school_id"];
 $query .= ";";
 
@@ -18,6 +18,8 @@ $numrows = pg_numrows($result);
   <tr>
    <th>USER ID</th>
    <th>TEACHER ID</th>
+   <th>USERNAME</th>
+   <th>PASSWORD</th>
    <th>FIRST NAME</th>
    <th>LAST NAME</th>
    <th>ROOM ID</th>
@@ -31,6 +33,8 @@ $numrows = pg_numrows($result);
     $row = pg_fetch_array($result, $ri);
     echo " <td>", $row["user_id"], "</td>
    <td>", $row["id"], "</td>
+   <td>", $row["username"], "</td>
+   <td>", $row["password"], "</td>
    <td>", $row["first_name"], "</td>
    <td>", $row["last_name"], "</td>
    <td>", $row["room_id"], "</td>
