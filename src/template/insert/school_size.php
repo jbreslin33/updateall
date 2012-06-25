3838b4ecabde9ea5a43ca890c94d85c035c6804b
jbreslin33 <jbreslin33@localhost>
2012-06-25 12:28:30 -0400

    added 3 letter words to password file. next i would like usernames to be these tiny words also.

--- src/template/insert/school_size.php
+++ src/template/insert/school_size.php
@@ -11,10 +11,10 @@ include("../links/links.php");
 <ul>
 <form name="insert" action="school.php" method="POST" >
 <h4>Number of Classes:</h4> 
-<input type="text" value="25" name="number_of_classes" />
+<input type="text" value="10" name="number_of_classes" />
 <br>
 <h4>Approximate Number of Students Per Class:</h4>
-<input type="text" value="35" name="number_of_students" />
+<input type="text" value="30" name="number_of_students" />
 <br>
 <input type="submit" value="Create School" />
 </form>
