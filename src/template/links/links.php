42b7d529c6331f13142c3b8c1edb6eaf6dbfad60
jbreslin33 <jbreslin33@localhost>
2012-06-27 19:48:20 -0400

    seperaratoin works. next: don't show inserts for non-admin.

--- src/template/links/links.php
+++ src/template/links/links.php
@@ -32,24 +32,9 @@ include("stats.php");
 include("inserts.php");
  ?>
 
+<?php
+include("selects.php");
+ ?>
 
-<p1>SELECT:<p1>
-<br>
-
-<a href="../select/student.php">SELECT STUDENT</a>
-<br>
-
-<a href="../select/teacher.php">SELECT TEACHER</a>
-<br>
-
-<a href="../select/class.php">SELECT CLASS</a>
-<br>
-
-<a href="../select/homeroom.php">SELECT HOMEROOM</a>
-<br>
-
-<a href="../select/school.php">SELECT SCHOOL</a>
-<br>
-<br>
 
 
