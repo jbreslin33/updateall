83438781cc088870fa666ac9ac38fa6d21308b98
jbreslin33 <jbreslin33@localhost>
2012-06-27 16:09:59 -0400

    i can now parse out school_name and username from login. next: actually log them in if valid and set session vars

--- src/login/login.php
+++ src/login/login.php
@@ -21,16 +21,48 @@ for ($i=0; $i < $arraySize; $i++)
         }
 }
 
+
+$school_name;
+$username;
+
+$before_period = true;
+$before_period_array = "";
+$after_period_array = "";
+
 //school attempt
 if ($period_count == 0)
 {
-	header("Location: login_form.php?message=no_periods");	
+	$_SESSION["school_name"] = $_POST["username"];
+	$_SESSION["username"] = "";
 }
 
 //non school attempt
 if ($period_count == 1)
 {
-	header("Location: login_form.php?message=one_period");	
+
+
+for ($i=0; $i < $arraySize; $i++)
+{
+        
+	if ($stringArray[$i] == '.')
+        {
+                $before_period = false;
+		continue;
+        }
+
+        if ($before_period)
+	{
+		$before_period_array .= $stringArray[$i];		
+	}
+	else
+	{
+		$after_period_array .= $stringArray[$i];		
+	}
+}
+	$_SESSION["school_name"] = $after_period_array;
+	$_SESSION["username"] = $before_period_array;
+
+
 }
 
 //invalid attempt
@@ -38,6 +70,11 @@ if ($period_count == 2)
 {
 	header("Location: login_form.php?message=too_many_periods");	
 }
+
+        echo $_SESSION["school_name"];
+	echo "<br>"; 
+        echo $_SESSION["username"];
+
 	pg_close();
 ?>
 
