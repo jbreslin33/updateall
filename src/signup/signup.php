c089ab0527eadda515178d02b7d5aa8289a1e2b2
jbreslin33 <jbreslin33@localhost>
2012-06-27 14:08:41 -0400

    works i think. next: print out stats in links page to make sure.

--- src/signup/signup.php
+++ src/signup/signup.php
@@ -181,10 +181,10 @@
   			
 			//----------------USER CHECK----------------------------------------------
         		//query string
-        		$query = "select id, first_name, last_name, school_id from users where school_id = ";
+        		$query = "select id, first_name, last_name from users where school_id = ";
         		$query .= $school_id;
         		$query .= " and ";
-        		$query .= "username = '';";
+        		$query .= "username is null;";
 
         		//get db result
         		$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
@@ -192,9 +192,6 @@
 
         		//get numer of rows
         		$num = pg_num_rows($result);
-
-        		//start new session
-        		//session_start();
 
         		// if there is a row then the username and password pair exists
         		if ($num > 0)
@@ -208,6 +205,7 @@
                 		$_SESSION["Login"] = "YES";
 
                 		//set user id, and subject levels to be used later
+                		$_SESSION["user_id"] = $id;
                 		$_SESSION["first_name"] = $first_name;
                 		$_SESSION["last_name"] = $last_name;
 
@@ -225,7 +223,7 @@
 			//--------------------INSERT INTO ADMINS----------------
                         //query string
                         $query = "INSERT INTO admins (user_id) VALUES (";
-                        $query .= $id;
+                        $query .= $_SESSION["user_id"];
                         $query .= ");";
 
                         // insert into users......
@@ -235,7 +233,7 @@
                         //--------------------INSERT INTO TEACHERS----------------
                         //query string
                         $query = "INSERT INTO teachers (user_id) VALUES (";
-                        $query .= $id;
+                        $query .= $_SESSION["user_id"];
                         $query .= ");";
 
                         // insert into users......
@@ -245,7 +243,7 @@
                         //--------------------INSERT INTO STUDENTS----------------
                         //query string
                         $query = "INSERT INTO students (user_id) VALUES (";
-			$query .= $id;
+                        $query .= $_SESSION["user_id"];
                         $query .= ");";
 
                         // insert into users......
@@ -257,8 +255,8 @@
  			//----------------STUDENT CHECK----------------------------------------------
         		//is this user a student? if so let's set some session vars
         		//query string
-        		$query = "select math_level, english_level from students where id = ";
-        		$query .= $_SESSION["id"];
+        		$query = "select id, math_level, english_level from students where user_id = ";
+        		$query .= $_SESSION["user_id"];
         		$query .= ";";
 
         		//get db result
@@ -274,10 +272,12 @@
                 		$_SESSION["is_student"] = "TRUE";
 
                 		//get the id from user table
+                		$student_id = pg_Result($result, 0, 'id');
                 		$mathLevel = pg_Result($result, 0, 'math_level');
                 		$englishLevel = pg_Result($result, 0, 'english_level');
 
                 		//set user id, and subject levels to be used later
+                		$_SESSION["student_id"] = $student_id;
                 		$_SESSION["math_level"] = $mathLevel;
                 		$_SESSION["english_level"] = $englishLevel;
         		}
@@ -292,8 +292,8 @@
   			//-------------------ADMIN CHECK-------------------------------------------
         		//is this user an admin ? if so let's set some session vars
         		//query string
-        		$query = "select id from admins where id = ";
-        		$query .= $_SESSION["id"];
+        		$query = "select id from admins where user_id = ";
+        		$query .= $_SESSION["user_id"];
         		$query .= ";";
 
         		//get db result
@@ -305,7 +305,11 @@
 
         		if ($num > 0)
         		{
-                		//we are an admin
+                		//get the id from user table
+                		$admin_id = pg_Result($result, 0, 'id');
+                		
+				//we are an admin
+                		$_SESSION["admin_id"] = $admin_id;
                 		$_SESSION["is_admin"] = "TRUE";
         		}
         		else
@@ -318,7 +322,7 @@
         		//is this user a teacher ? if so let's set some session vars
         		//query string
         		$query = "select id from teachers where id = ";
-        		$query .= $_SESSION["id"];
+        		$query .= $_SESSION["user_id"];
         		$query .= ";";
 
         		//get db result
@@ -330,7 +334,11 @@
 
         		if ($num > 0)
         		{
-                		//we are a teacher
+                		//get the id from user table
+                		$teacher_id = pg_Result($result, 0, 'id');
+                		
+				//we are a teacher
+                		$_SESSION["teacher_id"] = $teacher_id;
                 		$_SESSION["is_teacher"] = "TRUE";
         		}
         		else
