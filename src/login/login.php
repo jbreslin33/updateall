5be13ca8e4e2152fcb27ee3920b4dee8d89ee157
jbreslin33 <jbreslin33@localhost>
2012-06-27 18:30:00 -0400

    we are logging in as the last signup. a bug. login.php must not be selecting right stuff of one the vars is not being set.

--- src/login/login.php
+++ src/login/login.php
@@ -123,15 +123,15 @@ if ($period_count == 2)
         {
 
                 //get the id from user table
-                $school_id = pg_Result($result, 0, 'id');
+                $user_id = pg_Result($result, 0, 'id');
                 $first_name = pg_Result($result, 0, 'first_name');
                 $last_name = pg_Result($result, 0, 'last_name');
 
                 //set user id, and subject levels to be used later
-                $_SESSION["school_id"] = $school_id;
+                $_SESSION["user_id"] = $school_id;
                 $_SESSION["first_name"] = $first_name;
                 $_SESSION["last_name"] = $last_name;
-                header("Location: ../template/main/main.php");
+                //header("Location: ../template/main/main.php");
         }
         else
         {
@@ -139,7 +139,102 @@ if ($period_count == 2)
                 header("Location: login_form.php?message=no_user");
         }
 
+//now we need to select the rest of the tables now not we have a legit user so we can properly fill out session vars and thus overwrite old values.
+ //-------------------ADMIN CHECK-------------------------------------------
+                        //is this user an admin ? if so let's set some session vars
+                        //query string
+                        $query = "select id from admins where user_id = ";
+                        $query .= $_SESSION["user_id"];
+                        $query .= ";";
 
+                        //get db result
+                        $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+                        dbErrorCheck($conn,$result);
+
+                        //get numer of rows
+                        $num = pg_num_rows($result);
+
+                        if ($num > 0)
+                        {
+                                //get the id from user table
+                                $admin_id = pg_Result($result, 0, 'id');
+
+                                //we are an admin
+                                $_SESSION["admin_id"] = $admin_id;
+                                $_SESSION["is_admin"] = "TRUE";
+                        }
+                        else
+                        {
+                                //we are not an admin
+                                $_SESSION["is_admin"] = "FALSE";
+                        }
+
+                        //----------------TEACHER CHECK----------------------------------------------
+                        //is this user a teacher ? if so let's set some session vars
+                        //query string
+                        $query = "select id from teachers where id = ";
+                        $query .= $_SESSION["user_id"];
+                        $query .= ";";
+
+                        //get db result
+                        $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+                        dbErrorCheck($conn,$result);
+
+                        //get numer of rows
+                        $num = pg_num_rows($result);
+
+                        if ($num > 0)
+                        {
+                                //get the id from user table
+                                $teacher_id = pg_Result($result, 0, 'id');
+
+                                //we are a teacher
+                                $_SESSION["teacher_id"] = $teacher_id;
+                                $_SESSION["is_teacher"] = "TRUE";
+                        }
+                        else
+                        {
+                                //we are not a teacher
+                                $_SESSION["is_teacher"] = "FALSE";
+                        }
+  //----------------STUDENT CHECK----------------------------------------------
+                        //is this user a student? if so let's set some session vars
+                        //query string
+                        $query = "select id, math_level, english_level from students where user_id = ";
+                        $query .= $_SESSION["user_id"];
+                        $query .= ";";
+
+                        //get db result
+                        $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+                        dbErrorCheck($conn,$result);
+
+                        //get numer of rows
+                        $num = pg_num_rows($result);
+
+                        if ($num > 0)
+                        {
+                                //we are a student
+                                $_SESSION["is_student"] = "TRUE";
+
+                                //get the id from user table
+                                $student_id = pg_Result($result, 0, 'id');
+                                $mathLevel = pg_Result($result, 0, 'math_level');
+                                $englishLevel = pg_Result($result, 0, 'english_level');
+
+                                //set user id, and subject levels to be used later
+                                $_SESSION["student_id"] = $student_id;
+                                $_SESSION["math_level"] = $mathLevel;
+                                $_SESSION["english_level"] = $englishLevel;
+                        }
+                        else
+                        {
+                                //we are not a student
+                                $_SESSION["is_student"] = "FALSE";
+                                $_SESSION["math_level"] = "";
+                                $_SESSION["english_level"] = "";
+                        }
+
+                header("Location: ../template/main/main.php");
 pg_close();
 ?>
 
