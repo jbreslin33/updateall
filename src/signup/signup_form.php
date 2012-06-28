bdcae0f213c8725117d405eea10e95f9eb738c92
jbreslin33 <jbreslin33@localhost>
2012-06-28 09:46:07 -0400

    ridding files. dont' work but thes are gone

--- src/signup/signup_form.php
+++ src/signup/signup_form.php
@@ -6,7 +6,44 @@
 </head>
 
 <body>
-	<p><b> TO SIGN UP USERNAME MUST NOT CONTAIN NUMBERS</p></b>
+    header("Location: ../signup/signup_form.php?message=name_taken");
+                }
+                if ($space)
+                {
+                        header("Location: ../signup/signup_form.php?message=no_spaces");
+                }
+                if ($number)
+                {
+                        header("Location: ../signup/signup_form.php?message=no_numbers");
+                }
+                if ($period)
+                {
+                        header("Location: ../signup/signup_form.php?message=no_periods");
+                }
+                if ($_SESSION["school_name"] == '')
+                {
+                        header("Location: ../signup/signup_form.php?message=no_name");
+
+<?php
+      	$mess = $_GET["message"];
+
+	if ($mess == "name_taken")
+        {
+                echo "That username is taken, Please try another.";
+        }
+        if ($mess == "no_numbers")
+        {
+                echo "You cannot use numbers in username. Please try again.";
+        }
+        if ($mess == "no_periods")
+        {
+                echo "You cannot use periods in signup. Please try again.";
+        }
+        if ($mess == "no_name")
+        {
+                echo "You forgot a username. Please fill one out.";
+        }
+?>
 	<p><b> SIGN UP: </p></b>
 	
 	<form method="post" action="../signup/signup.php">
