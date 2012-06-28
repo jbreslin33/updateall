80066364389acc2d531d3274581bee5af02b7a9f
jbreslin33 <jbreslin33@localhost>
2012-06-28 09:35:51 -0400

    u

---
+++ src/signup/signup_form.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+	<title>Login</title>
+
+</head>
+
+<body>
+	<p><b> TO SIGN UP USERNAME MUST NOT CONTAIN NUMBERS</p></b>
+	<p><b> SIGN UP: </p></b>
+	
+	<form method="post" action="../signup/signup.php">
+
+	<p>Username: <input type="text" name="schoolname" /></p>
+	<p>Password: <input type="text" name="password" /></p>
+
+	<p><input type="submit" value="Sign Up" /></p>
+
+	</form>
+	
+</body>
+
+</html>
+
