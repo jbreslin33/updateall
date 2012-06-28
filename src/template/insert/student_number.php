a8ab8123bc7b2e8f56847d30e8433e6a23d3faa9
jbreslin33 <jbreslin33@localhost>
2012-06-27 20:10:41 -0400

    now sending just username in and not apppening. next: make it so non admins cannot insert

--- src/template/insert/student_number.php
+++ src/template/insert/student_number.php
@@ -3,11 +3,6 @@ include("../headers/header.php");
 
 session_start();
 
-
-//we first need some info, we need to know the username of admin 
-$school_name = $_SESSION["school_name"]; 
-$school_id = $_SESSION["school_id"]; 
-
 //first we need all the passwords then we can pick one at random
 $query = "select password from passwords;";
 $result = pg_query($conn,$query);
@@ -17,7 +12,10 @@ $randomNumber = rand(0,$numberOfRows);
 $password = pg_fetch_result($result, $randomNumber, password);
 
 //next we need to know what user we are up to for this school 
-$query = "select username from users where school_id = $school_id;";
+$query = "select username from users where school_id = ";
+$query .= $_SESSION["school_id"];
+$query .= ";";
+
 $result = pg_query($query);
 dbErrorCheck($conn,$result);
 $numberOfRows = pg_num_rows($result);
@@ -28,12 +26,10 @@ $userExtensionNumber = $numberOfRows;
 
 //now let's combine school_name and userExtensionNumber to come up with a new username.
 $newUsername = $userExtensionNumber;
-$newUsername .= ".";
-$newUsername .= $school_name; 
 
 //let's actually add the user
 $query = "INSERT INTO users (username,password,school_id) VALUES ('$newUsername','$password',";
-$query .= $school_id;
+$query .= $_SESSION["school_id"];
 $query .= ");";
 $result = pg_query($query);
 dbErrorCheck($conn,$result);
