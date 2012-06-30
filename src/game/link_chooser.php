dfa02a62b9fc38d960995275bd1422588fd1dc0e
jbreslin33 <jbreslin33@localhost>
2012-06-30 10:29:33 -0400

    added a link_chooser. i am going to make this generic so that it just takes you to link you bump

---
+++ src/game/link_chooser.php
@@ -0,0 +1,43 @@
+var GameChooser = new Class(
+{
+
+Extends: Game,
+
+        initialize: function(skill)
+        {
+                //application
+                this.parent(skill);
+        },
+
+	resetGame: function()
+	{
+		this.parent();
+
+		//let's reset all quiz stuff right here.
+                if (this.mQuiz)
+		{ 
+			this.mQuiz.reset();
+		}
+	},
+	
+	evaluateCollision: (function(col1,col2)
+        {
+	        this.parent(col1,col2);
+
+		//you ran into a question shape lets resolve it	
+		if (col1.mMessage == "controlObject" && col2.mMessage == "question")
+		{
+                        if (this.mQuiz)
+			{ 
+                       		this.mOn = false;
+                             	//this.setFeedback("YOU CHOSE WELL!!!");
+				window.location = col2.mQuestion.getAnswer();
+				
+			}	
+                }
+ 	}).protect()
+
+});
+
+
+
