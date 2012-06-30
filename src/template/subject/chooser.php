37aed614e0784b9c4580cb99f4d917a1fc086e9b
jbreslin33 <jbreslin33@localhost>
2012-06-30 15:37:59 -0400

    hud is now a class again

--- src/template/subject/chooser.php
+++ src/template/subject/chooser.php
@@ -53,6 +53,7 @@ var username = "<?php echo $username; ?>";
 <script type="text/javascript" src="../../div/div.php"></script>
 <script type="text/javascript" src="../../question/question.php"></script>
 <script type="text/javascript" src="../../quiz/quiz.php"></script>
+<script type="text/javascript" src="../../hud/hud.php"></script>
 
 </head>
 
@@ -74,45 +75,8 @@ window.addEvent('domready', function()
         document.addEvent("keydown", mApplication.keyDown);
         document.addEvent("keyup", mApplication.keyUp);
 
- /******************* BOUNDARY WALLS AND HUD COMBO ***********/
-        var y = 35;
-        northBoundsABCANDYOU = new Shape(120, y,  0,  0,"","","","red","boundary");
-        northBoundsABCANDYOU.setText('ABCANDYOU');
 
-        northBoundsUser = new Shape     (120, y,120,  0,"","","","orange","boundary");
-        northBoundsUser.setText('User : ' + username);
-
-        northBoundsMathLevel = new Shape(120, y,240,  0,"","","","yellow","boundary");
-        northBoundsMathLevel.setText('Level : ');
-
-        northBoundsScore = new Shape    (120, y,360,  0,"","","","LawnGreen","boundary");
-        northBoundsScore.setText('Score : ');
-
-        northBoundsScoreNeeded = new Shape    (120, y,480,  0,"","","","cyan","boundary");
-        northBoundsScoreNeeded.setText('Score Needed : ');
-
-        northBoundsGameName = new Shape (170, y,600,  0,"","","","#DBCCE6","boundary");
-        northBoundsGameName.setText('Choose Subject');
-
-        eastBounds  = new Shape         ( 10, 50,760, 35,"","","","#F8CDF8","boundary");
-        eastBounds  = new Shape         ( 10, 50,760, 85,"","","","#F6C0F6","boundary");
-        eastBounds  = new Shape         ( 10, 50,760,135,"","","","#F5B4F5","boundary");
-        eastBounds  = new Shape         ( 10, 50,760,185,"","","","#F6C0F6","boundary");
-        eastBounds  = new Shape         ( 10, 50,760,235,"","","","#F5B4F5","boundary");
-        eastBounds  = new Shape         ( 10, 50,760,285,"","","","#F3A8F3","boundary");
-        eastBounds  = new Shape         ( 10, 50,760,335,"","","","#F19BF1","boundary");
-        eastBounds  = new Shape         ( 10, 20,760,385,"","","","#F08EF0","boundary");
-
-        southBoundsQuestion = new Shape (770, y,  0,405,"","","","violet","boundary");
-
-        westBounds  = new Shape         ( 10, 50,  0, 35,"","","","#F8CDF8","boundary");
-        westBounds  = new Shape         ( 10, 50,  0, 85,"","","","#F6C0F6","boundary");
-        westBounds  = new Shape         ( 10, 50,  0,135,"","","","#F5B4F5","boundary");
-        westBounds  = new Shape         ( 10, 50,  0,185,"","","","#F6C0F6","boundary");
-        westBounds  = new Shape         ( 10, 50,  0,235,"","","","#F5B4F5","boundary");
-        westBounds  = new Shape         ( 10, 50,  0,285,"","","","#F3A8F3","boundary");
-        westBounds  = new Shape         ( 10, 50,  0,335,"","","","#F19BF1","boundary");
-        westBounds  = new Shape         ( 10, 20,  0,385,"","","","#F08EF0","boundary");
+	mHud = new Hud();
 
 	//the game
         mGame = new LinkChooser("Subject Chooser");
