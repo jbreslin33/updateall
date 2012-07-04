077341c74ebfe0c236295009d05fad6dd714160f
jbreslin33 <jbreslin33@localhost>
2012-07-04 16:40:58 -0400

    u

--- src/login/login.php
+++ src/login/login.php
@@ -234,15 +234,39 @@ $query .= ";";
 			 	echo "error no transactions";	
                         }
 
-/***get the last_level using last_level_id
+/***get the last_level using last_level_id ************/
 $query = "select level from levels where id = ";
 $query .= $_SESSION["last_level_id"];
 $query .= ";"; 
 
+ $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+                        dbErrorCheck($conn,$result);
+
+                        //get numer of rows
+                        $num = pg_num_rows($result);
+
+                        if ($num > 0)
+                        {
+                                //get the id from user table
+                                $last_level = pg_Result($result, 0, 'level');
+
+                                //set level_id
+                                $_SESSION["last_level"] = $last_level;
+                        }
+                        else
+                        {
+                                // no transaction in level_transactions so set level_id to 1
+                                echo "error no transactions";
+                        }
+
+
+
 //*****************************
   //---------------- GET next level id using last_level_id----------------------------------------------
 /*
-                $query = "select id from levels where level >order by level LIMIT 2;";
+                $query = "select id from levels where level > ";
+		$query .= $_SESSION["last_level"];
+		$query .= " order by level LIMIT 1;";
 
                 //get db result
                 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
