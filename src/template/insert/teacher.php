702e27d54deebf7e61deba969f64df462ef8c585
jbreslin33 <jbreslin33@localhost>
2012-07-08 17:39:27 -0400

    insert and select teacher works.

--- src/template/insert/teacher.php
+++ src/template/insert/teacher.php
@@ -1,9 +1,12 @@
 <?php 
 include("../headers/header.php");
+include("../../database/insert_into_users.php"); 
+include("../../database/insert_into_students.php"); 
+include("../../database/insert_into_teachers.php"); 
+include("../../database/insert_first_level_transaction.php"); 
+include("../../database/select_user_id.php"); 
 
-//we first need some info, we need to know the username of admin 
-$school_name = $_SESSION["school_name"]; 
-$school_id = $_SESSION["school_id"]; 
+session_start();
 
 //first we need all the passwords then we can pick one at random
 $query = "select password from passwords;";
@@ -14,7 +17,10 @@ $randomNumber = rand(0,$numberOfRows);
 $password = pg_fetch_result($result, $randomNumber, password);
 
 //next we need to know what user we are up to for this school 
-$query = "select username from users where school_id = $school_id;";
+$query = "select username from users where school_id = ";
+$query .= $_SESSION["school_id"];
+$query .= ";";
+
 $result = pg_query($query);
 dbErrorCheck($conn,$result);
 $numberOfRows = pg_num_rows($result);
@@ -25,50 +31,20 @@ $userExtensionNumber = $numberOfRows;
 
 //now let's combine school_name and userExtensionNumber to come up with a new username.
 $newUsername = $userExtensionNumber;
-$newUsername .= ".";
-$newUsername .= $school_name; 
 
 //let's actually add the user
-$query = "INSERT INTO users (username,password,school_id) VALUES ('$newUsername','$password',";
-$query .= $school_id;
-$query .= ");";
-$result = pg_query($query);
-dbErrorCheck($conn,$result);
+insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
 
-//now we need to add to students table as well
-$query = "select id from users where username = '$newUsername';";
-$result = pg_query($query);
-dbErrorCheck($conn,$result);
+//get new user id
+$new_user_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
 
-//get numer of rows
-$num = pg_num_rows($result);
-$new_id;
+//insert student
+insertIntoStudents($conn,$new_user_id);
 
-// if there is a row then the username and password pair exists
-if ($num > 0)
-{
-	//get the id from user table
-        $new_id = pg_Result($result, 0, 'id');
+//insert teacher 
+insertIntoTeachers($conn,$new_user_id);
 
-}
-else
-{
-	echo "no student id";
-}
-
-//now we need to insert into students table
-$query = "INSERT INTO students (user_id) VALUES (";
-$query .= $new_id;
-$query .= ");";
-$result = pg_query($query);
-dbErrorCheck($conn,$result);
-
-//now we need to insert into teachers table
-$query = "INSERT INTO teachers (user_id) VALUES (";
-$query .= $new_id;
-$query .= ");";
-$result = pg_query($query);
-dbErrorCheck($conn,$result);
+insertFirstLevelTransaction($conn,$new_user_id);
 
 //go to success page
 header("Location: ../select/teacher.php");
--- src/template/select/teacher.php
+++ src/template/select/teacher.php
@@ -4,7 +4,7 @@ include("../links/links.php");
 
 echo "<b><u>My Teachers:<u><b><br>";
 
-$query = "select teachers.user_id, teachers.id, users.username, users.password, users.first_name, users.last_name, teachers.room_id from teachers join users on teachers.user_id = users.id where users.school_id = ";
+$query = "select teachers.id,  users.username, users.password, users.first_name, users.last_name from teachers join users on teachers.id = users.id where users.school_id = ";
 $query .= $_SESSION["school_id"];
 $query .= ";";
 
@@ -16,13 +16,11 @@ $numrows = pg_numrows($result);
 
 <table border="1">
   <tr>
-   <th>USER ID</th>
-   <th>TEACHER ID</th>
+   <th>ID</th>
    <th>USERNAME</th>
    <th>PASSWORD</th>
    <th>FIRST NAME</th>
    <th>LAST NAME</th>
-   <th>ROOM ID</th>
   </tr>
 
 <?
@@ -31,13 +29,11 @@ $numrows = pg_numrows($result);
    for($ri = 0; $ri < $numrows; $ri++) {
     echo "<tr>\n";
     $row = pg_fetch_array($result, $ri);
-    echo " <td>", $row["user_id"], "</td>
-   <td>", $row["id"], "</td>
+    echo " <td>", $row["id"], "</td>
    <td>", $row["username"], "</td>
    <td>", $row["password"], "</td>
    <td>", $row["first_name"], "</td>
    <td>", $row["last_name"], "</td>
-   <td>", $row["room_id"], "</td>
   </tr>
   ";
    }
