058158f2894b3cf519cd832cde10c88ceb4a1f8d
jbreslin33 <jbreslin33@localhost>
2012-07-08 17:24:59 -0400

    u

--- src/template/insert/student_number.php
+++ src/template/insert/student_number.php
@@ -28,7 +28,19 @@ $userExtensionNumber = $numberOfRows;
 $newUsername = $userExtensionNumber;
 
 //let's actually add the user
+  //insert user
+/*
+                insertIntoUsers($conn,$_SESSION["username"], $_SESSION["password"], $_SESSION["school_id"]);
+                $new_user_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
 
+                //insert teacher
+                insertIntoTeachers($conn,$new_user_id);
+
+                //insert student
+                insertIntoStudents($conn,$new_user_id);
+
+                insertFirstLevelTransaction($conn,$new_user_id"]);
+*/
 
 
 
