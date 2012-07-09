372fd5af0b202c687c65ff984b2cef273a290e6b
jbreslin33 <jbreslin33@localhost>
2012-07-09 11:49:01 -0400

    left out include on select user

--- src/template/insert/teacher.php
+++ src/template/insert/teacher.php
@@ -1,12 +1,12 @@
 <?php 
 include("../headers/header.php");
-include("../../database/insert_into_users.php"); 
-include("../../database/insert_into_students.php"); 
-include("../../database/insert_into_teachers.php"); 
-include("../../database/insert_first_level_transaction.php"); 
-include("../../database/select_user_id.php"); 
 include("../../database/get_random_password.php"); 
 include("../../database/get_next_usernumber.php"); 
+include("../../database/insert_into_users.php"); 
+include("../../database/select_user_id.php"); 
+include("../../database/insert_into_teachers.php"); 
+include("../../database/insert_into_students.php"); 
+include("../../database/insert_first_level_transaction.php"); 
 
 session_start();
 
@@ -22,12 +22,13 @@ insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
 //get new user id
 $new_user_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
 
-//insert student
-insertIntoStudents($conn,$new_user_id);
-
 //insert teacher 
 insertIntoTeachers($conn,$new_user_id);
 
+//insert student
+insertIntoStudents($conn,$new_user_id);
+
+//insert first transaction for levels to lowest level
 insertFirstLevelTransaction($conn,$new_user_id);
 
 //go to success page
