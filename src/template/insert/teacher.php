bb92dc238b5cf6ec79fecc715ec0eba2b9b3d331
jbreslin33 <jbreslin33@localhost>
2012-07-09 11:33:05 -0400

    getRandomPassword function is created and used.

--- src/template/insert/teacher.php
+++ src/template/insert/teacher.php
@@ -5,16 +5,11 @@ include("../../database/insert_into_students.php");
 include("../../database/insert_into_teachers.php"); 
 include("../../database/insert_first_level_transaction.php"); 
 include("../../database/select_user_id.php"); 
+include("../../database/get_random_password.php"); 
 
 session_start();
 
-//first we need all the passwords then we can pick one at random
-$query = "select password from passwords;";
-$result = pg_query($conn,$query);
-dbErrorCheck($conn,$result);
-$numberOfRows = pg_num_rows($result);
-$randomNumber = rand(0,$numberOfRows);
-$password = pg_fetch_result($result, $randomNumber, password);
+$password = getRandomPassword($conn);
 
 //next we need to know what user we are up to for this school 
 $query = "select username from users where school_id = ";
@@ -31,6 +26,9 @@ $userExtensionNumber = $numberOfRows;
 
 //now let's combine school_name and userExtensionNumber to come up with a new username.
 $newUsername = $userExtensionNumber;
+
+
+
 
 //let's actually add the user
 insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
