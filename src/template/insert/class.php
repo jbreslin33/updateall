732b8cadf6143668027656964dd5e95d1e71a909
jbreslin33 <jbreslin33@localhost>
2012-07-09 12:25:25 -0400

    add class works.

--- src/template/insert/class.php
+++ src/template/insert/class.php
@@ -20,25 +20,40 @@ $newUsername = getNextUsernumber($conn);
 insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
 
 //get new user id
-$new_user_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
+$new_teacher_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
 
 //insert student
-insertIntoTeachers($conn,$new_user_id,0);
+insertIntoTeachers($conn,$new_teacher_id);
 
 //insert student
-insertIntoStudents($conn,$new_user_id);
+insertIntoStudents($conn,$new_teacher_id,$new_teacher_id);
 
 //insert first transaction for levels to lowest level
-insertFirstLevelTransaction($conn,$new_user_id);
+insertFirstLevelTransaction($conn,$new_teacher_id);
 
 
 //now loop thru and add a class of students..
 $number_of_students = $_POST["number_of_students"]; 
 for ($i = 0; $i < $number_of_students; $i++)
 {
+	//get a password
+	$password = getRandomPassword($conn);
+
+	//get a username
+	$newUsername = getNextUsernumber($conn);
+
+	//let's actually add the user
+	insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
+
+	//get new user id
+	$new_student_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
+
+	//insert student
+	insertIntoStudents($conn,$new_student_id,$new_teacher_id);
 
 
-
+	//insert first transaction for levels to lowest level
+	insertFirstLevelTransaction($conn,$new_student_id);
 }
 
 //go to success page
