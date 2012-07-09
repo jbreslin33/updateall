0c7ee49b4387bfde92089b8b1504f824071e0324
jbreslin33 <jbreslin33@localhost>
2012-07-09 17:45:25 -0400

    u

--- src/template/main/main.php
+++ src/template/main/main.php
@@ -1,7 +1,7 @@
 <?php 
 //standard header for most games i hope. it handles some basic html and level db call
 include("../headers/header.php");
-include("../links/links.php");
+include("../navigation/navigation_top_bar.php");
 
 ?>
 
