ab9ea1a4813db918420e745464292fa5e32d8e1d
jbreslin33 <jbreslin33@localhost>
2012-07-10 18:17:04 -0400

    clean up. next: use this strat to do all files with includes and that need style sheets.

--- src/template/main/main.php
+++ src/template/main/main.php
@@ -3,21 +3,14 @@
 <html>
 
 <head>
-
 <link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/updateall/src/css/green_style.css" />
 </head>
 
 <body>
-
 <?php
-//$link = "../navigation/navigation_top_bar_links.php";
-echo getenv("DOCUMENT_ROOT");
-
-$link = "/home/jbreslin/public_html/updateall/src/template/navigation/navigation_top_bar_links.php";
-//include($link);
 include(getenv("DOCUMENT_ROOT") . "/updateall/src/template/navigation/navigation_top_bar_links.php");
 ?>
-
 </body>
+
 </html>
 
