175fd985baed625c73b3538622fa863d3ec0bb3a
jbreslin33 <jbreslin33@localhost>
2012-07-10 18:27:58 -0400

    u

--- src/signup/signup.php
+++ src/signup/signup.php
@@ -1,20 +1,20 @@
-<?php include("../database/db_connect.php"); ?>
-<?php include("../database/set_level_session_variables.php"); ?>
-<?php include("../database/insert_into_schools.php"); ?>
-<?php include("../database/insert_into_users.php"); ?>
-<?php include("../database/insert_into_teachers.php"); ?>
-<?php include("../database/insert_into_students.php"); ?>
-<?php include("../database/insert_first_level_transaction.php"); ?>
-<?php include("../database/check_for_periods.php"); ?>
-<?php include("../database/check_for_spaces.php"); ?>
-<?php include("../database/check_for_numbers.php"); ?>
-<?php include("../database/check_for_schools.php"); ?>
-<?php include("../database/select_school_id.php"); ?>
-<?php include("../database/select_user_id.php"); ?>
-<?php include("../database/select_teacher_id.php"); ?>
-<?php include("../database/select_student_id.php"); ?>
-
 <?php
+include("../database/db_connect.php"); 
+include("../database/set_level_session_variables.php"); 
+include("../database/insert_into_schools.php"); 
+include("../database/insert_into_users.php"); 
+include("../database/insert_into_teachers.php"); 
+include("../database/insert_into_students.php"); 
+include("../database/insert_first_level_transaction.php"); 
+include("../database/check_for_periods.php");
+include("../database/check_for_spaces.php"); 
+include("../database/check_for_numbers.php"); 
+include("../database/check_for_schools.php"); 
+include("../database/select_school_id.php"); 
+include("../database/select_user_id.php"); 
+include("../database/select_teacher_id.php"); 
+include("../database/select_student_id.php"); 
+
 	//db connection
 	$conn = dbConnect();
         
