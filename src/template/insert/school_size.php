88cb812d54dfa27e2025fbc77b5c45712b7cd134
jbreslin33 <jbreslin33@localhost>
2012-07-10 22:40:53 -0400

    u next:

--- src/template/insert/school_size.php
+++ src/template/insert/school_size.php
@@ -1,11 +1,23 @@
-<?php 
-//standard header for most games i hope. it handles some basic html and level db call
-include("../headers/header.php");
+<!DOCTYPE html>
 
-?>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/updateall/src/css/green_style.css" />
 </head>
 
 <body>
+<?php
+session_start();
+//db connection
+include("../../database/db_connect.php");
+$conn = dbConnect();
+
+include(getenv("DOCUMENT_ROOT") . "/updateall/src/template/navigation/top_bar_links.php");
+echo "<br>";
+include(getenv("DOCUMENT_ROOT") . "/updateall/src/template/navigation/insert_links.php");
+?>
+
 <h2>Enter School Information :</h2>
 <ul>
 <form name="insert" action="school.php" method="POST" >
