ba468dfb7ce3c379544959a8c7ad5c1f7a2eef45
jbreslin33 <jbreslin33@localhost>
2012-07-11 20:11:37 -0400

    u

--- src/database/goto_next_level.php
+++ src/database/goto_next_level.php
@@ -17,7 +17,7 @@ $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error())
 setLevelSessionVariables($conn,$_SESSION["user_id"]);
 
 //send player to the game page where he will be redirected.
-header("Location: /updateall/src/template/game/chooser.php");
+header("Location: /updateall/web/game/chooser.php");
 
 ?>
 
