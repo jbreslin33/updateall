ec321d8222655661fa830e4093c672748659d7ae
jbreslin33 <jbreslin33@localhost>
2012-07-11 20:21:45 -0400

    next: stats,insert,select past: logout

--- web/navigation/top_links.php
+++ web/navigation/top_links.php
@@ -4,7 +4,7 @@
 <li><a href="../navigation/insert.php">Inserts</a></li>
 <li><a href="../navigation/select.php">Selects</a></li>
 <li><a href="../stats/stats.php">Stats</a></li>
-<li><a href="../../login/login_form.php">Logout</a></li>
+<li><a href="/updateall/web/login/login_form.php">Logout</a></li>
 </ul>
 
 
