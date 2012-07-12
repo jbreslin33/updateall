5cf1a54296fe00b9312d1cd487d6e2650b2255c6
jbreslin33 <jbreslin33@localhost>
2012-07-11 20:30:35 -0400

    u

---
+++ web/insert/insert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/updateall/css/green_block.css" />
+</head>
+
+<body>
+<?php
+include(getenv("DOCUMENT_ROOT") . "/updateall/web/navigation/top_links.php");
+echo "<br>";
+include(getenv("DOCUMENT_ROOT") . "/updateall/src/template/navigation/insert_links.php");
+?>
+</body>
+
+</html>
+
+
--- web/navigation/top_links.php
+++ web/navigation/top_links.php
@@ -1,8 +1,8 @@
 <ul>
 <li><a href="/updateall/web/home/home.php">Home</a></li>
 <li><a href="/updateall/web/game/chooser.php">Play</a></li>
-<li><a href="../navigation/insert.php">Inserts</a></li>
-<li><a href="../navigation/select.php">Selects</a></li>
+<li><a href="/updateall/web/insert/insert.php">Inserts</a></li>
+<li><a href="/updateall/web/select/select.php">Selects</a></li>
 <li><a href="/updateall/web/stats/stats.php">Stats</a></li>
 <li><a href="/updateall/web/login/login_form.php">Logout</a></li>
 </ul>
