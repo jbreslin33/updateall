680f91d9e0de5e2c207c7ffc567a549d7065fd50
jbreslin33 <jbreslin33@localhost>
2012-07-13 13:49:09 -0400

    u

--- src/hud/hud.php
+++ src/hud/hud.php
@@ -11,26 +11,26 @@ var Hud = new Class(
 	/******************* BOUNDARY WALLS AND HUD COMBO ***********/
         var ySize = 35;
         var yCoord = 0;
-        this.mHome = new Shape(60, ySize,  0,  yCoord,"","","","#F8CDF8","boundary");
+        this.mHome = new Shape(40, ySize,  0,  yCoord,"","","","#F8CDF8","boundary");
         this.mHome.setText('<font size="1"> <a href="<?php getenv("DOCUMENT_ROOT")?>/web/home/home.php"> HOME</a> </font>');
 
-	this.mLogout = new Shape     (60, ySize,60,  yCoord,"","","","red","boundary");
+	this.mLogout = new Shape     (50, ySize,40,  yCoord,"","","","red","boundary");
         this.mLogout.setText('<font size="1"> <a href="<?php getenv("DOCUMENT_ROOT")?>/web/login/login_form.php"> LOGOUT</a> </font>');
 	
-	this.mUsername = new Shape     (120, ySize,120,  yCoord,"","","","orange","boundary");
-        this.mUsername.setText('<font size="2"> user : ' + username + '</font>');
-        
-	this.mLevel = new Shape(120, ySize,240,  yCoord,"","","","yellow","boundary");
+        this.mGameName = new Shape (200, ySize,90,  yCoord,"","","","orange","boundary");
+	
+	this.mLevel = new Shape(100, ySize,290,  yCoord,"","","","yellow","boundary");
         this.mLevel.setText('<font size="2"> Level : ' + next_level + '</font>');
 
-        this.mScore = new Shape    (120, ySize,360,  yCoord,"","","","LawnGreen","boundary");
+        this.mScore = new Shape    (100, ySize,390,  yCoord,"","","","LawnGreen","boundary");
         this.mScore.setText('<font size="2"> Score : </font>');
 
-        this.mScoreNeeded = new Shape    (120, ySize, 480,  yCoord,"","","","cyan","boundary");
+        this.mScoreNeeded = new Shape    (100, ySize, 490,  yCoord,"","","","cyan","boundary");
 
-        this.mGameName = new Shape (170, ySize,600,  yCoord,"","","","#F8CDF8","boundary");
-
-        eastBounds  = new Shape         ( 10, 50,760, 35,"","","","#F8CDF8","boundary");
+	this.mUsername = new Shape     (180, ySize,590,  yCoord,"","","","#F8CDF8","boundary");
+        this.mUsername.setText('<font size="2"> User : ' + username + '</font>');
+        
+	eastBounds  = new Shape         ( 10, 50,760, 35,"","","","#F8CDF8","boundary");
         eastBounds  = new Shape         ( 10, 50,760, 85,"","","","#F6C0F6","boundary");
         eastBounds  = new Shape         ( 10, 50,760,135,"","","","#F5B4F5","boundary");
         eastBounds  = new Shape         ( 10, 50,760,185,"","","","#F6C0F6","boundary");
