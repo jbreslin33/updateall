76002305b632cf9bbe54174acc5873034652dcc3
jbreslin33 <jbreslin33@localhost>
2012-07-13 13:30:40 -0400

    u

--- src/hud/hud.php
+++ src/hud/hud.php
@@ -9,25 +9,26 @@ var Hud = new Class(
         initialize: function()
         {
 	/******************* BOUNDARY WALLS AND HUD COMBO ***********/
-        var y = 35;
-        this.mHome = new Shape(60, y,  0,  0,"","","","#F8CDF8","boundary");
+        var ySize = 35;
+        var yCoord = 35;
+        this.mHome = new Shape(60, ySize,  0,  0,"","","","#F8CDF8","boundary");
         this.mHome.setText('<font size="1"> <a href="<?php getenv("DOCUMENT_ROOT")?>/web/home/home.php"> HOME</a> </font>');
 
-	this.mLogout = new Shape     (60, y,60,  0,"","","","red","boundary");
+	this.mLogout = new Shape     (60, ySize,60,  0,"","","","red","boundary");
         this.mLogout.setText('<font size="1"> <a href="<?php getenv("DOCUMENT_ROOT")?>/web/login/login_form.php"> LOGOUT</a> </font>');
 	
-	this.mUsername = new Shape     (120, y,120,  0,"","","","orange","boundary");
+	this.mUsername = new Shape     (120, ySize,120,  0,"","","","orange","boundary");
         this.mUsername.setText('<font size="2"> user : ' + username + '</font>');
         
-	this.mLevel = new Shape(120, y,240,  0,"","","","yellow","boundary");
+	this.mLevel = new Shape(120, ySize,240,  0,"","","","yellow","boundary");
         this.mLevel.setText('<font size="2"> Level : ' + next_level + '</font>');
 
-        this.mScore = new Shape    (120, y,360,  0,"","","","LawnGreen","boundary");
+        this.mScore = new Shape    (120, ySize,360,  0,"","","","LawnGreen","boundary");
         this.mScore.setText('<font size="2"> Score : </font>');
 
-        this.mScoreNeeded = new Shape    (120, y,480,  0,"","","","cyan","boundary");
+        this.mScoreNeeded = new Shape    (120, ySize, 480,  0,"","","","cyan","boundary");
 
-        this.mGameName = new Shape (170, y,600,  0,"","","","#F8CDF8","boundary");
+        this.mGameName = new Shape (170, ySize,600,  0,"","","","#F8CDF8","boundary");
 
         eastBounds  = new Shape         ( 10, 50,760, 35,"","","","#F8CDF8","boundary");
         eastBounds  = new Shape         ( 10, 50,760, 85,"","","","#F6C0F6","boundary");
@@ -38,7 +39,7 @@ var Hud = new Class(
         eastBounds  = new Shape         ( 10, 50,760,335,"","","","#F19BF1","boundary");
         eastBounds  = new Shape         ( 10, 20,760,385,"","","","#F08EF0","boundary");
 
-        this.mQuestion = new Shape (770, y,  0,405,"","","","violet","boundary");
+        this.mQuestion = new Shape (770, ySize,  0,405,"","","","violet","boundary");
 
         westBounds  = new Shape         ( 10, 50,  0, 35,"","","","#F8CDF8","boundary");
         westBounds  = new Shape         ( 10, 50,  0, 85,"","","","#F6C0F6","boundary");
