e2c9ffe1ba63555c0700e073f52f731f68c686b2
jbreslin33 <jbreslin33@localhost>
2012-07-15 20:29:00 -0400

    next: ??

--- src/database/cluster_db_build.sql
+++ src/database/cluster_db_build.sql
@@ -1660,7 +1660,31 @@ insert into levels_domains (level_id, domain_id) values (26,1);
 
 
 --LEVELS_GRADES
-
+insert into levels_grades (level_id, grade_id) values (1,1);
+insert into levels_grades (level_id, grade_id) values (3,1);
+insert into levels_grades (level_id, grade_id) values (4,1);
+insert into levels_grades (level_id, grade_id) values (5,1);
+insert into levels_grades (level_id, grade_id) values (6,1);
+insert into levels_grades (level_id, grade_id) values (7,1);
+insert into levels_grades (level_id, grade_id) values (8,1);
+insert into levels_grades (level_id, grade_id) values (9,1);
+insert into levels_grades (level_id, grade_id) values (10,1);
+insert into levels_grades (level_id, grade_id) values (11,1);
+insert into levels_grades (level_id, grade_id) values (12,1);
+insert into levels_grades (level_id, grade_id) values (13,1);
+insert into levels_grades (level_id, grade_id) values (14,1);
+insert into levels_grades (level_id, grade_id) values (15,1);
+insert into levels_grades (level_id, grade_id) values (16,1);
+insert into levels_grades (level_id, grade_id) values (17,1);
+insert into levels_grades (level_id, grade_id) values (18,1);
+insert into levels_grades (level_id, grade_id) values (19,1);
+insert into levels_grades (level_id, grade_id) values (20,1);
+insert into levels_grades (level_id, grade_id) values (21,1);
+insert into levels_grades (level_id, grade_id) values (22,1);
+insert into levels_grades (level_id, grade_id) values (23,1);
+insert into levels_grades (level_id, grade_id) values (24,1);
+insert into levels_grades (level_id, grade_id) values (25,1);
+insert into levels_grades (level_id, grade_id) values (26,1);
 
 
 --LEVELS_TRANSACTIONS
