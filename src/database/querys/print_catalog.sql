dd731ab17802f3c5257bf9b37c1f25a511659ed3
jbreslin33 <jbreslin33@localhost>
2012-07-19 09:28:15 -0400

    next: subject

--- src/database/querys/print_catalog.sql
+++ src/database/querys/print_catalog.sql
@@ -1,3 +1,3 @@
-select * from standards_clusters_domains_grades, clusters_domains_grades, standards, clusters, domains_grades, domains where clusters_domains_grades.id = standards_clusters_domains_grades.cluster_domain_grade_id and standards_clusters_domains_grades.standard_id = standards.id and clusters_domains_grades.cluster_id = clusters.id and clusters_domains_grades.domain_grade_id = domains_grades.id and domains_grades.domain_id = domains.id;
+select * from standards_clusters_domains_grades, clusters_domains_grades, standards, clusters, domains_grades, domains, grades where clusters_domains_grades.id = standards_clusters_domains_grades.cluster_domain_grade_id and standards_clusters_domains_grades.standard_id = standards.id and clusters_domains_grades.cluster_id = clusters.id and clusters_domains_grades.domain_grade_id = domains_grades.id and domains_grades.domain_id = domains.id and domains_grades.grade_id = grades.id;
 
 
