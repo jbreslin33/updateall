e70eb8fbf5f6cf4a8df5dc8d52ddf803903d1b41
jbreslin33 <jbreslin33@localhost>
2012-08-06 14:21:09 -0400

    adding player shape

---
+++ src/shape/shape_player.php
@@ -0,0 +1,15 @@
+var Player = new Class(
+{
+
+Extends: Shape,
+	initialize: function(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+        {
+		this.parent(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+        },
+
+	onCollision: function(col)
+	{
+	
+	}
+});
+
