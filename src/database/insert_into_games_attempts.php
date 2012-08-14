343fd4db66a1315a2e2578893047cd3c49886e39
bbreslin <bbreslin@localhost>
2012-08-13 21:28:51 -0400

    looks like score and start/end times are being updated correctly. i'm still hardcoding game_id though.

--- src/database/insert_into_games_attempts.php
+++ src/database/insert_into_games_attempts.php
@@ -12,9 +12,7 @@ function insertIntoGamesAttempts($conn,$curDate,$game_id,$user_id,$level_id)
 				//$mysqldate = date( 'Y-m-d H:i:s', $curDate );
 				//$mysqldate = date('Y-m-d H:i:s');
 				
-                $query = "INSERT INTO games_attempts (game_attempt_time_start,game_attempt_time_end,game_id,user_id,level_id) VALUES ('";
-				$query .= $curDate;
-                $query .= "','";
+                $query = "INSERT INTO games_attempts (game_attempt_time_start,game_id,user_id,level_id) VALUES ('";
 				$query .= $curDate;
                 $query .= "',";
 				$query .= $game_id;
@@ -27,35 +25,6 @@ function insertIntoGamesAttempts($conn,$curDate,$game_id,$user_id,$level_id)
                 // insert into users......
                 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
                 dbErrorCheck($conn,$result);
-                
-}
-
-//brian - update current score in games_attempts
-
-//$q=$_GET["q"];
-
-function insertScoreIntoGamesAttempts($conn,$user_id,$score,$curDate)
-{
- 		//--------------------INSERT INTO STUDENTS----------------
-                //query string 
-                //$query = "UPDATE games_attempts SET score =" . $score . "WHERE user_id" = . $user_id;
-				
-				$query = "UPDATE games_attempts SET score = " .  "'" . $score  .  "'" . "WHERE user_id = " .  "'" .  $user_id  .  "'" . "AND game_attempt_time_start = '";
-				$query .= $curDate;
-                $query .= "';";
-                //$query .= $user_id;
-                //$query .= ",";
-                //$query .= $level_id;
-                //$query .= ");";
-                
-                // insert into users......
-                $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
-                dbErrorCheck($conn,$result);
-				
-	
-			
-				//UPDATE book SET price = 19.49 WHERE price = 25.00
-				
                 
 }
 
