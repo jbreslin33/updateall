42d6d3f836cf9b6406e5edccb1fa022fcdc4878d
jbreslin33 <jbreslin33@localhost>
2012-08-14 18:42:15 -0400

    forgot a file

---
+++ src/shape/shape_busdriver.php
@@ -0,0 +1,11 @@
+var BusDriver = new Class(
+{
+
+Extends: Player,
+ 	initialize: function(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+        {
+                this.parent(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+        }
+
+});
+
