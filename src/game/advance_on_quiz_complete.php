c5d41057495334501d90fa60a60874aacaa13582
jbreslin33 <jbreslin33@localhost>
2012-08-23 20:28:21 -0400

    works now but there is flash of shapes after you enter door.

--- src/game/advance_on_quiz_complete.php
+++ src/game/advance_on_quiz_complete.php
@@ -5,19 +5,22 @@ Extends: Game,
         initialize: function(skill)
         {
                 this.parent(skill)
-        },
-	
+        }
+/*	
         update: function(delta)
         {
-
 		if (this.mQuiz.isQuizComplete())
 		{
-			mApplication.log('completed quiz');
                 	this.mOn = false;
-                	window.location = '/src/database/goto_next_level.php';
+                	//window.location = "../../src/database/goto_next_level.php";
+			window.location = /src/database/goto_next_level.php;
 		}
-		this.parent(delta);
+		else
+		{	
+			this.parent(delta);
+		}
 	}
+*/
 });
 
 
