b4452af6b66302e3fda241afc3f3a37dc1e5dd41
jbreslin33 <jbreslin33@localhost>
2012-09-23 12:02:03 -0400

    u

--- src/shape/shape_player.php
+++ src/shape/shape_player.php
@@ -22,21 +22,7 @@ Extends: Shape,
 	{
 
 	},
-/*
-	correctAnswer: function()
-	{
-        	//set text of control object
-                if (this.mGame.mQuiz)
-                {
-                	//set the control objects question object
-                        this.mGame.mControlObject.setQuestion(this.mGame.mQuiz.getQuestion());
-                        if (this.mMounteeArray[0])
-                        {
-                                this.mMounteeArray[0].setQuestion(this.mGame.mQuiz.getQuestion());
-                        }
-                }
-	},
-*/	
+	
 	incorrectAnswer: function()
 	{
 	}
