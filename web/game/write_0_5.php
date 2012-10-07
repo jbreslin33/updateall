38d2e26549daab23f3a7fe8aed94f2c918a8370a
jbreslin33 <jbreslin33@localhost>
2012-10-07 10:49:27 -0400

    fixed mounted numbers

--- web/game/write_0_5.php
+++ web/game/write_0_5.php
@@ -125,7 +125,35 @@ window.addEvent('domready', function()
         door.mUrl = '/src/database/goto_next_level.php';
         door.mOpenOnQuestionSolved = true;
         mGame.addToShapeArray(door);
-       
+      
+/*
+ //QUESTION SHAPES
+        count = 0;
+        for (i = startNumber + countBy; i <= endNumber; i = i + countBy)
+        {
+                var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
+                var shape;
+                mGame.addToShapeArray(shape = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,mQuiz.getSpecificQuestion(count),"/images/treasure/gold_coin_head.png","","question"));
+                shape.createMountPoint(0,-5,-41);
+                shape.showQuestion(false);
+
+                //numberMount to go on top let's make it small and draw it on top
+                var numberMountee = new Shape(1,1,100,100,mGame,mQuiz.getSpecificQuestion(count),"","orange","numberMountee");
+                numberMountee.setMountable(true);
+                mGame.addToShapeArray(numberMountee);
+                numberMountee.showQuestion(false);
+
+                //do the mount
+                shape.mount(numberMountee,0);
+
+                numberMountee.setBackgroundColor("transparent");
+
+                count++;
+        }
+
+*/
+
+ 
 	//QUESTION SHAPES 
         for (i = 0; i < scoreNeeded; i++)
         {
@@ -137,6 +165,7 @@ window.addEvent('domready', function()
 
 		//numberMount to go on top let's make it small and draw it on top 
                 var numberMountee = new Shape(1,1,100,100,mGame,mQuiz.getSpecificQuestion(i),"","orange","numberMountee");       
+                numberMountee.setMountable(true);
                 mGame.addToShapeArray(numberMountee); 
                 numberMountee.showQuestion(false);
                 
