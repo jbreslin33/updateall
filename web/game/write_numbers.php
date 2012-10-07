28a928d4e679f5881ab803c332cb91ec63168b45
jbreslin33 <jbreslin33@localhost>
2012-10-07 11:56:07 -0400

    u

--- web/game/write_numbers.php
+++ web/game/write_numbers.php
@@ -7,22 +7,65 @@
 <script type="text/javascript" src="/src/mootools/mootools-core-1.4.5-full-compat.js"></script>
 
 <?php
-
-
 include(getenv("DOCUMENT_ROOT") . "/web/login/check_login.php");
 include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
+include(getenv("DOCUMENT_ROOT") . "/src/database/insert_into_games_attempts.php");
+
+//db connection
+$conn = dbConnect();
+
+
+//query the game table, eventually maybe there will be more than one result here which would be a choice of game for that level.
+$query = "select score_needed, count_by, start_number, end_number from counting where level_id = ";
+$query .= $_SESSION["next_level_id"];
+$query .= ";";
+
+//get db result
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
 //game variables to fill from db
 $username = $_SESSION["username"];
 $next_level = $_SESSION["next_level"];
+$scoreNeeded = 0;
+$countBy = 0;
+$startNumber = 0;
+$endNumber = 0;
+
+//get numer of rows
+$num = pg_num_rows($result);
+
+// if there is a row then id exists it better be unique!
+if ($num > 0)
+{
+        //get row
+        $row = pg_fetch_row($result);
+
+        //fill php vars from db
+        $scoreNeeded = $row[0];
+        $countBy = $row[1];
+        $startNumber = $row[2];
+        $endNumber = $row[3];
+}
+
+//brian - get current date
+$_SESSION["game_start_time"] = date('Y-m-d H:i:s');
+$_SESSION["game_over"] = "false";
+
+//brian - attempt a game - still hardcoding game_id = 1
+insertIntoGamesAttempts($conn,$_SESSION["game_start_time"],1,$_SESSION["user_id"],$_SESSION["next_level"]);
 
 ?>
 
 <script language="javascript">
 
+var curDate = "<?php echo $curDate; ?>";
 var username = "<?php echo $username; ?>";
 var next_level = "<?php echo $next_level; ?>";
-var scoreNeeded = 5;
+var scoreNeeded = <?php echo $scoreNeeded; ?>;
+var countBy = <?php echo $countBy; ?>;
+var startNumber = <?php echo $startNumber; ?>;
+var endNumber = <?php echo $endNumber; ?>;
+
 
 </script>
 
@@ -66,7 +109,7 @@ window.addEvent('domready', function()
        	mHud = new Hud();
         mHud.mScoreNeeded.setText('<font size="2"> Needed : ' + scoreNeeded + '</font>');
 	mHud.mGameName.setText('<font size="2">DUNGEON</font>');	
-	
+
 	//GAME
         mGame = new Game("hardcode");
 
@@ -75,77 +118,83 @@ window.addEvent('domready', function()
 	mGame.mQuiz = mQuiz;
 
 	//QUESTIONS FOR QUIZ
-       	mQuiz.mQuestionArray.push(new Question('What comes next after 0 _', '1'));      
-       	mQuiz.mQuestionArray.push(new Question('What comes next after 0 1 _', '2'));      
-       	mQuiz.mQuestionArray.push(new Question('What comes next after 0 1 2 _', '3'));      
-       	mQuiz.mQuestionArray.push(new Question('What comes next after 0 1 2 3 _', '4'));      
-       	mQuiz.mQuestionArray.push(new Question('What comes next after 0 1 2 3 4 _', '5'));      
+	for (i = startNumber; i < endNumber; i = i + countBy)
+        {
+        	var question = new Question('What number comes after ' + i + '?', i + countBy);      
+                mQuiz.mQuestionArray.push(question);
+        }
 
+ 	
 	//CONTROL OBJECT
         mGame.mControlObject = new Player(50,50,400,300,mGame,mQuiz.getSpecificQuestion(0),"/images/characters/wizard.png","","controlObject");
-        mGame.mControlObject.mHideOnQuestionSolved = false;
- 	mGame.mControlObject.createMountPoint(0,-5,-41);
+	mGame.mControlObject.mHideOnQuestionSolved = false;
+	mGame.mControlObject.createMountPoint(0,-5,-41);
 
         //set animation instance
         mGame.mControlObject.mAnimation = new AnimationAdvanced(mGame.mControlObject);
 
-	mGame.mControlObject.mAnimation.addAnimations('/images/characters/wizard_','.png');
+ 	mGame.mControlObject.mAnimation.addAnimations('/images/characters/wizard_','.png');
 
  	mGame.addToShapeArray(mGame.mControlObject);
         mGame.mControlObject.showQuestionObject(false);
 
-        //numberMount to go on top let's make it small and draw it on top
+        //text question mountee
         var numberMountee = new Shape(100,50,300,300,mGame,mQuiz.getSpecificQuestion(0),"","orange","numberMountee");
-        numberMountee.setMountable(true);
- 	numberMountee.setHideOnDrop(true);
+	numberMountee.setMountable(true);
+	numberMountee.setHideOnDrop(true);
         mGame.addToShapeArray(numberMountee);
 
         //do the mount
         mGame.mControlObject.mount(numberMountee,0);
+
         numberMountee.setBackgroundColor("transparent");
 
-     	//question for key
-        var keyQuestion = new Question('Pick up key.',"key");
-        mQuiz.mQuestionArray.push(keyQuestion);
+	//question for key 
+	var keyQuestion = new Question('Pick up key.',"key");
+	mQuiz.mQuestionArray.push(keyQuestion);
 
-        //KEY
-        openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-        var key = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,keyQuestion,"/images/key/key_dungeon.gif","","key");
-        key.setVisibility(false);
-        key.mShowOnlyOnQuizComplete = true;
-        key.mMountable = true;
-        key.setHideOnQuestionSolved(false);
-        mGame.addToShapeArray(key);
+	//KEY
+       	openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
+ 	var key = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,keyQuestion,"/images/key/key_dungeon.gif","","key");
+	key.setVisibility(false);
+	key.mShowOnlyOnQuizComplete = true;
+	key.mMountable = true;
+	key.setHideOnQuestionSolved(false);
+	mGame.addToShapeArray(key);
 
-        //question for door
-        var doorQuestion = new Question('Open door with key.',"door");
-        mQuiz.mQuestionArray.push(doorQuestion);
+	//question for door 
+	var doorQuestion = new Question('Open door with key.',"door");
+	mQuiz.mQuestionArray.push(doorQuestion);
 
-        //DOOR
-        var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-        var door = new ShapeDoor(50,50,openPoint.mX,openPoint.mY,mGame,doorQuestion,"/images/doors/door_closed.png","","door","/images/doors/door_open.png");
-        door.mUrl = '/src/database/goto_next_level.php';
-        door.mOpenOnQuestionSolved = true;
-        mGame.addToShapeArray(door);
-      
+	//DOOR
+       	var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
+	var door = new ShapeDoor(50,50,openPoint.mX,openPoint.mY,mGame,doorQuestion,"/images/doors/door_closed.png","","door","/images/doors/door_open.png");
+	door.mUrl = '/src/database/goto_next_level.php';
+	door.mOpenOnQuestionSolved = true;
+	mGame.addToShapeArray(door);
+
 	//QUESTION SHAPES 
-        for (i = 0; i < scoreNeeded; i++)
+        count = 0;
+        for (i = startNumber + countBy; i <= endNumber; i = i + countBy)
         {
        		var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-                var shape;
-               	mGame.addToShapeArray(shape = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,mQuiz.getSpecificQuestion(i),"/images/treasure/gold_coin_head.png","","question"));
- 		shape.createMountPoint(0,-5,-41);
+               	var shape;
+               	mGame.addToShapeArray(shape = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,mQuiz.getSpecificQuestion(count),"/images/treasure/gold_coin_head.png","","question"));
+		shape.createMountPoint(0,-5,-41);
                 shape.showQuestion(false);
 
 		//numberMount to go on top let's make it small and draw it on top 
-                var numberMountee = new Shape(1,1,100,100,mGame,mQuiz.getSpecificQuestion(i),"","orange","numberMountee");       
-                numberMountee.setMountable(true);
+                var numberMountee = new Shape(1,1,100,100,mGame,mQuiz.getSpecificQuestion(count),"","orange","numberMountee");       
+		numberMountee.setMountable(true);
                 mGame.addToShapeArray(numberMountee); 
                 numberMountee.showQuestion(false);
                 
 		//do the mount  
 		shape.mount(numberMountee,0);
+
 		numberMountee.setBackgroundColor("transparent");
+
+                count++;
         }
 	
 	//CHASERS
@@ -153,8 +202,8 @@ window.addEvent('domready', function()
 	for (i = 0; i < chasers; i++)
         {
        		var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-                var aishape = new ShapeChaser(50,50,openPoint.mX,openPoint.mY,mGame,"","/images/monster/red_monster.png","","chaser");
-		mGame.addToShapeArray(aishape);
+                var shape = new ShapeChaser(50,50,openPoint.mX,openPoint.mY,mGame,"","/images/monster/red_monster.png","","chaser");
+		mGame.addToShapeArray(shape);
         }
 
 	//RESET GAME TO START
@@ -162,6 +211,9 @@ window.addEvent('domready', function()
 
         //START UPDATING
         var t=setInterval("mGame.update()",32)
+		
+		//brian - update score in database
+		var t=setInterval("mGame.updateScore()",1000)
 
 }
 );
