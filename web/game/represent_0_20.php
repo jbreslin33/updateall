332ee7fb79817301fdead75f74495045eb3155f3
jbreslin33 <jbreslin33@localhost>
2012-10-21 18:16:05 -0400

    added door and key. but game resets when i hit key or door i forget. i think key.

--- web/game/represent_0_20.php
+++ web/game/represent_0_20.php
@@ -114,11 +114,30 @@ window.addEvent('domready', function()
         mGame.mControlObject.mount(numberMountee,0);
         numberMountee.setBackgroundColor("transparent");
 
-	//DOOR
-       	var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-	var door = new ShapeDoor(50,50,openPoint.mX,openPoint.mY,mGame,new Question("DOOR","/src/database/goto_next_level.php"),"/images/doors/door_closed.png","","door","/images/doors/door_open.png");
-	mGame.addToShapeArray(door);
-               
+        //question for key
+        var keyQuestion = new Question('Pick up key.',"key");
+        mQuiz.mQuestionArray.push(keyQuestion);
+
+        //KEY
+        openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
+        var key = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,keyQuestion,"/images/key/key_dungeon.gif","","key");
+        key.setVisibility(false);
+        key.mShowOnlyOnQuizComplete = true;
+        key.mMountable = true;
+        key.setHideOnQuestionSolved(false);
+        mGame.addToShapeArray(key);
+
+     	//question for door
+        var doorQuestion = new Question('Open door with key.',"door");
+        mQuiz.mQuestionArray.push(doorQuestion);
+
+        //DOOR
+        var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
+        var door = new ShapeDoor(50,50,openPoint.mX,openPoint.mY,mGame,doorQuestion,"/images/doors/door_closed.png","","door","/images/doors/door_open.png");
+        door.mUrl = '/src/database/goto_next_level.php';
+        door.mOpenOnQuestionSolved = true;
+        mGame.addToShapeArray(door);
+ 
 	//QUESTION SHAPES 
         for (i = 0; i < scoreNeeded; i++)
         {
