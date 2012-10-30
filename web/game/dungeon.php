0da686999ba383dc133e6f17f69ee33683de548e
jbreslin33 <jbreslin33@localhost>
2012-10-30 19:09:45 -0400

    u

--- web/game/dungeon.php
+++ web/game/dungeon.php
@@ -8,8 +8,33 @@ $conn = dbConnect();
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_question_query.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_games_attempts.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_game_includes.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_top.php");
+?>
 
+<script language="javascript">
+var curDate = "<?php echo $curDate; ?>";
+var username = "<?php echo $username; ?>";
+var next_level = "<?php echo $next_level; ?>";
+
+</script>
+</head>
+
+<body bgcolor="grey">
+
+<script language="javascript">
+var mGame;
+var mApplication;
+
+window.addEvent('domready', function()
+{
+
+        //APPLICATION
+        mApplication = new ApplicationGameDungeon();
+
+        //KEYS
+        document.addEvent("keydown", mApplication.keyDown);
+        document.addEvent("keyup", mApplication.keyUp);
+
+<?php
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_bottom.php");
 ?>
 
