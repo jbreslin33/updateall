43f7743e1ef107c291a057a408920ae275547d09
jbreslin33 <jbreslin33@localhost>
2012-10-30 17:59:58 -0400

    addded game includes

--- web/game/standard_top.php
+++ web/game/standard_top.php
@@ -1,25 +1,10 @@
+
 <script language="javascript">
 var curDate = "<?php echo $curDate; ?>";
 var username = "<?php echo $username; ?>";
 var next_level = "<?php echo $next_level; ?>";
+
 </script>
-
-<script type="text/javascript" src="/src/math/point2D.php"></script>
-<script type="text/javascript" src="/src/bounds/bounds.php"></script>
-<script type="text/javascript" src="/src/game/game.php"></script>
-<script type="text/javascript" src="/src/application/application.php"></script>
-<script type="text/javascript" src="/src/animation/animation.php"></script>
-<script type="text/javascript" src="/src/animation/animation_advanced.php"></script>
-<script type="text/javascript" src="/src/shape/shape.php"></script>
-<script type="text/javascript" src="/src/shape/shape_player.php"></script>
-<script type="text/javascript" src="/src/shape/shape_door.php"></script>
-<script type="text/javascript" src="/src/shape/shape_ai.php"></script>
-<script type="text/javascript" src="/src/div/div.php"></script>
-<script type="text/javascript" src="/src/question/question.php"></script>
-<script type="text/javascript" src="/src/quiz/quiz.php"></script>
-<script type="text/javascript" src="/src/hud/hud.php"></script>
-
-
 </head>
 
 <body bgcolor="grey">
@@ -57,7 +42,3 @@ window.addEvent('domready', function()
                 var question = new Question(questions[i],answers[i]);
                 mQuiz.mQuestionArray.push(question);
         }
-
-
-
-
