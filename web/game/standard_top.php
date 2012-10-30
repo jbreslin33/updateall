8325ad0f6eedca0bedd905eeb9b460b28d500206
jbreslin33 <jbreslin33@localhost>
2012-10-30 15:42:42 -0400

    u

--- web/game/standard_top.php
+++ web/game/standard_top.php
@@ -1,15 +1,3 @@
-<?php
-
-//brian - get current date
-$_SESSION["game_start_time"] = date('Y-m-d H:i:s');
-$_SESSION["game_over"] = "false";
-
-include(getenv("DOCUMENT_ROOT") . "/src/database/insert_into_games_attempts.php");
-
-//brian - attempt a game - still hardcoding game_id = 1
-insertIntoGamesAttempts($conn,$_SESSION["game_start_time"],1,$_SESSION["user_id"],$_SESSION["next_level"]);
-?>
-
 <script language="javascript">
 var curDate = "<?php echo $curDate; ?>";
 var username = "<?php echo $username; ?>";
