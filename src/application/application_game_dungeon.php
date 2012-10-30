48876d77cba3fd3cb270f20449092377b777ebcb
jbreslin33 <jbreslin33@localhost>
2012-10-30 18:39:43 -0400

    u

--- src/application/application_game_dungeon.php
+++ src/application/application_game_dungeon.php
@@ -1,37 +1,12 @@
-var ApplicationGame = new Class(
+var ApplicationGameDungeon = new Class(
 {
 
-Extends: Application,
+Extends: ApplicationGame,
 
         initialize: function()
         {
                 this.parent();
 	
-	//KEYS
-        document.addEvent("keydown", mApplication.keyDown);
-        document.addEvent("keyup", mApplication.keyUp);
-
-        //BOUNDS AND HUD COMBO
-        mBounds = new Bounds(60,735,380,35);
-
-        mHud = new Hud();
-        mHud.mScoreNeeded.setText('<font size="2"> Needed : ' + scoreNeeded + '</font>');
-        mHud.mGameName.setText('<font size="2">DUNGEON</font>');
-
-        //GAME
-        mGame = new Game("hardcode");
-
-        //QUIZ
-        mQuiz = new Quiz(scoreNeeded);
-        mGame.mQuiz = mQuiz;
-
-        //QUESTIONS FOR QUIZ
-        for (i = 0; i < scoreNeeded; i++)
-        {
-                var question = new Question(questions[i],answers[i]);
-                mQuiz.mQuestionArray.push(question);
-        }
-
 
 
         },
--- web/game/standard_top.php
+++ web/game/standard_top.php
@@ -17,29 +17,9 @@ window.addEvent('domready', function()
 {
 
         //APPLICATION
-        mApplication = new ApplicationGame();
+        mApplication = new ApplicationGameDungeon();
+
         //KEYS
         document.addEvent("keydown", mApplication.keyDown);
         document.addEvent("keyup", mApplication.keyUp);
-/*
-        //BOUNDS AND HUD COMBO
-        mBounds = new Bounds(60,735,380,35);
 
-        mHud = new Hud();
-        mHud.mScoreNeeded.setText('<font size="2"> Needed : ' + scoreNeeded + '</font>');
-        mHud.mGameName.setText('<font size="2">DUNGEON</font>');
-
-        //GAME
-        mGame = new Game("hardcode");
-
-        //QUIZ
-        mQuiz = new Quiz(scoreNeeded);
-        mGame.mQuiz = mQuiz;
-
-        //QUESTIONS FOR QUIZ
-        for (i = 0; i < scoreNeeded; i++)
-        {
-                var question = new Question(questions[i],answers[i]);
-                mQuiz.mQuestionArray.push(question);
-        }
-*/
