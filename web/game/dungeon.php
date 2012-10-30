e245aa869138e88044208d7771de10bd11a17ab1
jbreslin33 <jbreslin33@localhost>
2012-10-30 19:05:23 -0400

    u

--- web/game/dungeon.php
+++ web/game/dungeon.php
@@ -9,12 +9,6 @@ include(getenv("DOCUMENT_ROOT") . "/web/game/standard_question_query.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_games_attempts.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_game_includes.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_top.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_game_constructors.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_control_object.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_key.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_door.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_question_shapes.php");
-include(getenv("DOCUMENT_ROOT") . "/web/game/standard_chasers.php?chasers=6");
 
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_bottom.php");
 ?>
