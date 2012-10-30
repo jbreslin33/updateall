a0f9f088e3f0088b0e92da20c51661fd57c6d4d4
jbreslin33 <jbreslin33@localhost>
2012-10-30 15:40:02 -0400

    u

--- web/game/dungeon.php
+++ web/game/dungeon.php
@@ -1,5 +1,11 @@
 <?php
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_title_mootools.php");
+include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
+
+//db connection
+$conn = dbConnect();
+
+include(getenv("DOCUMENT_ROOT") . "/web/game/standard_question_query.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_top.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_control_object.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_key.php");
--- web/game/standard_top.php
+++ web/game/standard_top.php
@@ -1,64 +1,10 @@
 <?php
-include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
-include(getenv("DOCUMENT_ROOT") . "/src/database/insert_into_games_attempts.php");
-
-//db connection
-$conn = dbConnect();
-
-//query the game table, eventually maybe there will be more than one result here which would be a choice of game for that level.
-$query = "select question, answer, question_order from questions where level_id = ";
-$query .= $_SESSION["next_level_id"];
-$query .= " ORDER BY question_order;";
-
-
-//get db result
-$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
-
-//game variables to fill from db
-$username = $_SESSION["username"];
-$next_level = $_SESSION["next_level"];
-
-//get numer of rows which will also be score needed
-$numberOfRows = pg_num_rows($result);
-$scoreNeeded = pg_num_rows($result);
-
-?>
-
-
-
-
-<?php
-
-echo "<script language=\"javascript\">";
-echo "var numberOfRows = $numberOfRows;";
-echo "var scoreNeeded = $numberOfRows;";
-echo "var questions = new Array();";
-echo "var answers = new Array();";
-
-echo "</script>";
-
-$counter = 0;
-while ($row = pg_fetch_row($result))
-{
-        //fill php vars from db
-        $questions = $row[0];
-        $answers = $row[1];
-
-        echo "<script language=\"javascript\">";
-
-        echo "questions[$counter] = \"$questions\";";
-        echo "answers[$counter] = \"$answers\";";
-        echo "</script>";
-        $counter++;
-}
-
-
-
-
 
 //brian - get current date
 $_SESSION["game_start_time"] = date('Y-m-d H:i:s');
 $_SESSION["game_over"] = "false";
+
+include(getenv("DOCUMENT_ROOT") . "/src/database/insert_into_games_attempts.php");
 
 //brian - attempt a game - still hardcoding game_id = 1
 insertIntoGamesAttempts($conn,$_SESSION["game_start_time"],1,$_SESSION["user_id"],$_SESSION["next_level"]);
