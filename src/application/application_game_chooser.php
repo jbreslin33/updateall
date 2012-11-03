c6d53d3227dd4b4cb28a95137d575fc7d8699577
jbreslin33 <jbreslin33@localhost>
2012-11-03 19:39:36 -0400

    u

--- src/application/application_game_chooser.php
+++ src/application/application_game_chooser.php
@@ -1,4 +1,4 @@
-var ApplicationGameDungeon = new Class(
+var ApplicationGameChooser = new Class(
 {
 
 Extends: ApplicationGame,
@@ -11,18 +11,20 @@ Extends: ApplicationGame,
 		this.createControlObject("/images/characters/wizard.png");
 
 		//create key
-		this.createKey("/images/key/key_dungeon.gif");
+		//this.createKey("/images/key/key_dungeon.gif");
 	
 		//create door	
-		this.createDoor("/images/doors/door_closed.png","/images/doors/door_open.png");
+//		var numberOfRows = 1;
+//		for (i = 1; i < numberOfRows + 1; i++)
+//		{ 
+			this.createDoor("/images/doors/door_closed.png","/images/doors/door_open.png");
+//		}
 	},
 
 	createControlObject: function(image_source)
 	{
 		//*******************CONTROL OBJECT
-        	mGame.mControlObject = new Player(50,50,400,300,mGame,mQuiz.getSpecificQuestion(0),image_source,"","controlObject");
-        	mGame.mControlObject.mHideOnQuestionSolved = false;
-        	mGame.mControlObject.createMountPoint(0,-5,-41);
+        	mGame.mControlObject = new Player(50,50,400,300,mGame,'',image_source,"","controlObject");
 
         	//set animation instance
         	mGame.mControlObject.mAnimation = new AnimationAdvanced(mGame.mControlObject);
@@ -30,46 +32,11 @@ Extends: ApplicationGame,
         	mGame.mControlObject.mAnimation.addAnimations('/images/characters/wizard_','.png');
 
         	mGame.addToShapeArray(mGame.mControlObject);
-        	mGame.mControlObject.showQuestionObject(false);
-
-        	//text question mountee
-        	var questionMountee = new Shape(100,50,300,300,mGame,mQuiz.getSpecificQuestion(0),"","orange","questionMountee");
-        	questionMountee.setMountable(true);
-        	questionMountee.setHideOnDrop(true);
-        	mGame.addToShapeArray(questionMountee);
-        	mGame.mControlObject.setStartingMountee(questionMountee);
-
-        	//do the mount
-        	mGame.mControlObject.mount(questionMountee,0);
-
-        	questionMountee.setBackgroundColor("transparent");
-        	questionMountee.showQuestion(true);
 	},
 
 	createQuestionShapes: function(image_source)
 	{
-                count = 0;
-                for (i = 0; i < numberOfRows; i++)
-                {
-                        var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-                        var shape;
-                        mGame.addToShapeArray(shape = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,mQuiz.getSpecificQuestion(count),image_source,"","question"));
-                        shape.createMountPoint(0,-5,-41);
-                        shape.showQuestion(false);
-
-                        //numberMount to go on top let's make it small and draw it on top
-                        var questionMountee = new Shape(1,1,100,100,mGame,mQuiz.getSpecificQuestion(count),"","orange","questionMountee");
-                        questionMountee.setMountable(true);
-                        mGame.addToShapeArray(questionMountee);
-                        questionMountee.showQuestion(false);
-
-                        //do the mount
-                        shape.mount(questionMountee,0);
-
-                        questionMountee.setBackgroundColor("transparent");
-
-                        count++;
-                }
+	
 	},
 
 	createChasers: function(image_source)
@@ -83,20 +50,6 @@ Extends: ApplicationGame,
 
 	},
 
-	createKey: function(image_source)
-	{
-        	var keyQuestion = new Question('Pick up key.',"key");
-        	mQuiz.mQuestionArray.push(keyQuestion);
-
-        	openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
-        	var key = new Shape(50,50,openPoint.mX,openPoint.mY,mGame,keyQuestion,"/images/key/key_dungeon.gif","","key");
-        	key.setVisibility(false);
-        	key.mShowOnlyOnQuizComplete = true;
-       	 	key.mMountable = true;
-        	key.setHideOnQuestionSolved(false);
-        	mGame.addToShapeArray(key);
-	},
-
 	createDoor: function(image_source_closed,image_source_open)
 	{
         	var doorQuestion = new Question('Open door with key.',"door");
@@ -104,7 +57,9 @@ Extends: ApplicationGame,
 
         	var openPoint = mGame.getOpenPoint2D(40,735,75,375,50,7);
         	var door = new ShapeDoor(50,50,openPoint.mX,openPoint.mY,mGame,doorQuestion,image_source_closed,"","door",image_source_open);
-        	door.mUrl = '/src/database/goto_next_level.php';
+        	//door.mUrl = '/src/database/goto_next_level.php';
+		//door.mUrl = url[i-1];
+		door.mUrl = url[0];
         	door.mOpenOnQuestionSolved = true;
         	mGame.addToShapeArray(door);
 
@@ -113,7 +68,25 @@ Extends: ApplicationGame,
 
 		//create chasers
 		this.createChasers("/images/monster/red_monster.png");
+	},
+
+        createHud: function()
+        {
+        	mHud = new Hud();
+        	mHud.mScoreNeeded.setText('<font size="2"> Needed : 1 </font>');
+        	mHud.mGameName.setText('<font size="2">GAME CHOOSER</font>');
+        },
+
+	createQuiz: function()
+	{
+                mQuiz = new Quiz(1);
+                mGame.mQuiz = mQuiz;
+	},
+
+	createQuestions: function()
+	{
+	
 	}
+
 		
 });
-
