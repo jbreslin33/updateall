b412cc2c6be3f816e6ce5e11781df21dcd7e16dc
jbreslin33 <jbreslin33@localhost>
2012-11-03 22:14:26 -0400

    moved includes out of standard game includes

--- web/game/dungeon.php
+++ web/game/dungeon.php
@@ -12,6 +12,8 @@ include(getenv("DOCUMENT_ROOT") . "/web/game/standard_games_attempts.php");
 include(getenv("DOCUMENT_ROOT") . "/web/game/standard_game_includes.php");
 ?>
 
+<script type="text/javascript" src="/src/application/application_game_dungeon.php"></script>
+
 <!-- HUD VARIABLES    -->
 <script language="javascript">
 var curDate = "<?php echo $curDate; ?>";
