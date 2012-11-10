30afc83b9834d07459d3477181d2c9cc68cb2a79
jbreslin33 <jbreslin33@localhost>
2012-11-10 16:00:44 -0500

    fixing up inheritance.

--- src/application/application_game.php
+++ src/application/application_game.php
@@ -21,6 +21,9 @@ Extends: Application,
 
 		//create questions
 		this.createQuestions();
+
+                //create control object
+                this.createControlObject("/images/characters/wizard.png");
         },
 
 	createBounds: function(north,east,south,west)
@@ -57,7 +60,16 @@ Extends: Application,
 
         createControlObject: function(image_source)
         {
-        
+                //*******************CONTROL OBJECT
+                mGame.mControlObject = new Player(50,50,400,300,mGame,mQuiz.getSpecificQuestion(0),image_source,"","controlObject");
+
+                //set animation instance
+                mGame.mControlObject.mAnimation = new AnimationAdvanced(mGame.mControlObject);
+                mGame.mControlObject.mAnimation.addAnimations('/images/characters/wizard_','.png');
+                mGame.addToShapeArray(mGame.mControlObject);
+                
+		mGame.mControlObject.mHideOnQuestionSolved = false;
+                mGame.mControlObject.createMountPoint(0,-5,-41);
 	},
 
         createQuestionShapes: function()
