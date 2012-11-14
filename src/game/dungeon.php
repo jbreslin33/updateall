e6d7b6d18960dad45fd5d50e7f3819a07d84c962
jbreslin33 <jbreslin33@localhost>
2012-11-13 19:50:28 -0500

    u

--- src/game/dungeon.php
+++ src/game/dungeon.php
@@ -14,52 +14,6 @@ Extends: Game,
 		this.createDoor("/images/doors/door_closed.png","/images/doors/door_open.png");
 	},
 
-	createControlObject: function(image_source)
-	{
-		this.parent(image_source);
-        	this.mControlObject.createMountPoint(0,-5,-41);
-        	this.mControlObject.showQuestionObject(false);
-        	
-		//text question mountee
-        	var questionMountee = new Shape(100,50,300,300,this,mQuiz.getSpecificQuestion(0),"","orange","questionMountee");
-        	this.addToShapeArray(questionMountee);
-
-        	//do the mount
-        	this.mControlObject.mount(questionMountee,0);
-
-        	questionMountee.setBackgroundColor("transparent");
-        	questionMountee.showQuestion(true);
-	},
-
-	createQuestionShapes: function()
-	{
-                count = 0;
-                for (i = 0; i < numberOfRows; i++)
-                {
-                        var openPoint = this.getOpenPoint2D(40,735,75,375,50,7);
-                        var shape;
-                        this.addToShapeArray(shape = new Shape(50,50,openPoint.mX,openPoint.mY,this,mQuiz.getSpecificQuestion(count),"/images/treasure/gold_coin_head.png","","question"));
-                        shape.createMountPoint(0,-5,-41);
-                        shape.showQuestion(false);
-
-                        //numberMount to go on top let's make it small and draw it on top
-                        var questionMountee = new Shape(1,1,100,100,this,mQuiz.getSpecificQuestion(count),"","orange","questionMountee");
-                        this.addToShapeArray(questionMountee);
-                        questionMountee.showQuestion(false);
-
-                        //do the mount
-                        shape.mount(questionMountee,0);
-
-                        questionMountee.setBackgroundColor("transparent");
-
-                        count++;
-                }
-	},
-
-	createChasers: function()
-	{
-	},
-
 	createKey: function(image_source)
 	{
         	var keyQuestion = new Question('Pick up key.',"key");
@@ -84,6 +38,5 @@ Extends: Game,
         	this.addToShapeArray(door);
 
 	}
-		
 });
 
