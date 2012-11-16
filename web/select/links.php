28ea2485b4eb37e1e27104197cb2e94026bc36f8
bbreslin99 <bbreslin99@localhost>
2012-11-16 13:13:11 -0500

    putting in my changes

--- web/select/links.php
+++ web/select/links.php
@@ -3,4 +3,6 @@
 <li><a href="/web/select/teacher.php">Select Teachers</a></li>
 <li><a href="/web/select/class.php">Select Classes</a></li>
 <li><a href="/web/select/school.php">Select Schools</a></li>
+<li><a href="/web/select/student_tracker.php">Student Tracker</a>
+<li><a href="/web/select/select_info.php">View My Info</a></li>
 </ul>
