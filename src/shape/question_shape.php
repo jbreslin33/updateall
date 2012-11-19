9ee7c825091eddcb914663066361ca10ac57a225
jbreslin33 <jbreslin33@localhost>
2012-11-19 16:59:05 -0500

    u

--- src/shape/question_shape.php
+++ src/shape/question_shape.php
@@ -42,10 +42,6 @@ Extends: Shape,
 
         evaluateQuestions: function(col)
         {
-		//evaluate answers to questions provided both shapes have questions.
-                var answer = 0;
-                var answerCol = 0;
-                
 		if (this.getEvaluateQuestions())
                 {
                         if (this.mQuestion && col.mQuestion)
