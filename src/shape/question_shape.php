bbed7c7efef85c9b55c55f3cfe900ff78670a22c
jbreslin33 <jbreslin33@localhost>
2012-11-18 19:44:26 -0500

    extending dungeon type shapes from questionshape

---
+++ src/shape/question_shape.php
@@ -0,0 +1,10 @@
+var QuestionShape = new Class(
+{
+
+Extends: Shape,
+	initialize: function(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+        {
+		this.parent(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+        }
+
+});
--- web/game/standard_game_includes.php
+++ web/game/standard_game_includes.php
@@ -6,6 +6,7 @@
 <script type="text/javascript" src="/src/animation/animation.php"></script>
 <script type="text/javascript" src="/src/animation/animation_advanced.php"></script>
 <script type="text/javascript" src="/src/shape/shape.php"></script>
+<script type="text/javascript" src="/src/shape/question_shape.php"></script>
 <script type="text/javascript" src="/src/shape/shape_player.php"></script>
 <script type="text/javascript" src="/src/shape/shape_door.php"></script>
 <script type="text/javascript" src="/src/shape/shape_ai.php"></script>
