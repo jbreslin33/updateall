00f1d227185e5e900a48bed81de6fc70d83fe1b2
jbreslin33 <jbreslin33@localhost>
2012-11-18 19:46:24 -0500

    moving question and quiz stuff out of base shape class to questionShape

--- src/shape/question_shape.php
+++ src/shape/question_shape.php
@@ -5,6 +5,12 @@ Extends: Shape,
 	initialize: function(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
         {
 		this.parent(width,height,spawnX,spawnY,game,question,src,backgroundColor,message)
+
+                //we are all about questions and answers in this program so everyone contains a question pointer whether you use it or not.
+                this.mQuestion = question; // the question object that contains a question and answer.
+                this.mShowQuestionObject = true; //even if we have a valid question object we can shut off showing it.
+                this.mShowQuestion = true; //toggles between question or answer text from question object
+
         }
 
 });
