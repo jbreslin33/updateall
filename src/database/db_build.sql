c64ae4a2a015c1684048f62e2d84ce9283ea4ee4
jbreslin33 <jbreslin33@localhost>
2012-11-21 09:14:38 -0500

    fixing levels

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1516,6 +1516,7 @@ insert into standards_clusters_domains_grades (standard_id, cluster_domain_grade
 --Know number names and the count sequence.
 
 
+----------------------------------------------------------------------------
 --   	1.	 Count to 100 by ones and by tens.
 ----------------------------------------------------------------------------
 insert into levels(level,description) values (1,'Start of Journey'); --1      
@@ -1532,45 +1533,142 @@ insert into levels(level,description) values (11,'Count from 90 to 100');      -
 
 insert into levels(level,description) values (12,'Count to 100 by tens');      --12 
 
-
+--------------------------------------------------------------------------------
 --   	2.	 Count forward beginning from a given number within the known
 --   	sequence (instead of having to begin at 1).
 --------------------------------------------------------------------------------
 
 
 
+--------------------------------------------------------------------------------
 --   	3.	 Write numbers from 0 to 20. Represent a number of objects with a
 --	written numeral 0-20 (with 0 representing a count of no objects).
 --	Count to tell the number of objects. 	
+--------------------------------------------------------------------------------
 
+
+
+--------------------------------------------------------------------------------
 --	4.	 Understand the relationship between numbers and quantities; connect
 --	counting to cardinality.
+--------------------------------------------------------------------------------
 
+
+
+--------------------------------------------------------------------------------
 --	a.	 When counting objects, say the number names in the standard
 --	order, pairing each object with one and only one number name
 --	and each number name with one and only one object.
+--------------------------------------------------------------------------------
 
+
+
+--------------------------------------------------------------------------------
 --	b.	 Understand that the last number name said tells the number of
 --	objects counted. The number of objects is the same regardless of
 --	their arrangement or the order in which they were counted.
+--------------------------------------------------------------------------------
 
+
+
+--------------------------------------------------------------------------------
 --	c.	 Understand that each successive number name refers to a quantity
 --	that is one larger.
+--------------------------------------------------------------------------------
 
+
+
+--------------------------------------------------------------------------------
 --	5.	 Count to answer “how many?” questions about as many as 20 things
 --	arranged in a line, a rectangular array, or a circle, or as many as 10
 --	things in a scattered configuration; given a number from 1–20, count
 --	out that many objects.
 --	Compare numbers.
+--------------------------------------------------------------------------------
 
+
+
+
+--------------------------------------------------------------------------------
 --	6.	 Identify whether the number of objects in one group is greater than,
 --	less than, or equal to the number of objects in another group, e.g., by
 --	using matching and counting strategies.1
+--------------------------------------------------------------------------------
 
+
+
+--------------------------------------------------------------------------------
 --	7.	 Compare two numbers between 1 and 10 presented as written
 --	numerals.
+--------------------------------------------------------------------------------
 
 
+
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+
+
+
+--	Operations and Algebraic Thinking                      K.OA	
+--	Understand addition as putting together and adding to, and under-
+--	stand subtraction as taking apart and taking from.
+--	1.	 Represent addition and subtraction with objects, fingers, mental
+--	images, drawings2, sounds (e.g., claps), acting out situations, verbal
+--	explanations, expressions, or equations.
+--	2.	 Solve addition and subtraction word problems, and add and subtract
+--	within 10, e.g., by using objects or drawings to represent the problem.
+--	3.	 Decompose numbers less than or equal to 10 into pairs in more
+--	than one way, e.g., by using objects or drawings, and record each
+--	decomposition by a drawing or equation (e.g., 5 = 2 + 3 and 5 = 4 + 1).
+--	4.	 For any number from 1 to 9, find the number that makes 10 when
+--	added to the given number, e.g., by using objects or drawings, and
+--	record the answer with a drawing or equation.
+--	5.	 Fluently add and subtract within 5.
+--	Include groups with up to ten objects.
+--	Drawings need not show details, but should show the mathematics in the problem.
+--	(This applies wherever drawings are mentioned in the Standards.)
+
+
+
+--	Number and Operations in Base Ten  K.NBT	
+--	Work with numbers 11–19 to gain foundations for place value.
+--	1.	 Compose and decompose numbers from 11 to 19 into ten ones and
+--	some further ones, e.g., by using objects or drawings, and record each
+--	composition or decomposition by a drawing or equation (e.g., 18 = 10 +
+--	8); understand that these numbers are composed of ten ones and one,
+--	two, three, four, five, six, seven, eight, or nine ones.
+--	Measurement and Data	
+--	K.MD
+--	Describe and compare measurable attributes.
+--	1.	 Describe measurable attributes of objects, such as length or weight.
+--	Describe several measurable attributes of a single object.
+--	2.	 Directly compare two objects with a measurable attribute in common,
+--	to see which object has “more of”/“less of” the attribute, and describe
+--	the difference. For example, directly compare the heights of two
+--	children and describe one child as taller/shorter.
+--	Classify objects and count the number of objects in each category.
+--	3.	 Classify objects into given categories; count the numbers of objects in
+--	each category and sort the categories by count.3
+--	Geometry	
+--	K.G
+--	Identify and describe shapes (squares, circles, triangles, rectangles,
+--	hexagons, cubes, cones, cylinders, and spheres).
+--	1.	 Describe objects in the environment using names of shapes, and
+--	describe the relative positions of these objects using terms such as
+--	above, below, beside, in front of, behind, and next to.
+--	2.	 Correctly name shapes regardless of their orientations or overall size.
+--	3.	 Identify shapes as two-dimensional (lying in a plane, “flat”) or three-
+--	dimensional (“solid”).
+--	Analyze, compare, create, and compose shapes.
+--	4.	 Analyze and compare two- and three-dimensional shapes, in
+--	different sizes and orientations, using informal language to describe
+--	their similarities, differences, parts (e.g., number of sides and
+--	vertices/“corners”) and other attributes (e.g., having sides of equal
+--	length).
+--	5.	 Model shapes in the world by building shapes from components (e.g.,
+--	sticks and clay balls) and drawing shapes.
+--	6.	 Compose simple shapes to form larger shapes. For example, “Can you
+--	join these two triangles with full sides touching to make a rectangle?”
 
 
 --count from random known sequence
