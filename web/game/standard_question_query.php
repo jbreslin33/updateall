17f4a6d719259d96c4c33deba215e27e432bb53b
jbreslin33 <jbreslin33@localhost>
2012-11-23 22:06:08 -0500

    u

--- web/game/standard_question_query.php
+++ web/game/standard_question_query.php
@@ -5,7 +5,9 @@ echo "var questions = new Array();";
 echo "var answers = new Array();";
 echo "</script>";
 
-//query the game table, eventually maybe there will be more than one result here which would be a choice of game for that level.
+
+
+//*******************     anything in questions?
 $query = "select question, answer, question_order from questions where level_id = ";
 $query .= $_SESSION["next_level_id"];
 $query .= " ORDER BY question_order;";
@@ -14,9 +16,20 @@ $query .= " ORDER BY question_order;";
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
 //get numer of rows which will also be score needed
-$numberOfRows = pg_num_rows($result);
+$numberOfRowsInQuestions = pg_num_rows($result);
 
-if ($numberOfRows > 0)
+
+//*******************     anything in counting?
+$query = "select score_needed, start_number, end_number, count_by from counting where level_id = ";
+$query .= $_SESSION["next_level_id"];
+
+//get db result
+$result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
+
+//get numer of rows which will also be score needed
+$numberOfRowsInCounting = pg_num_rows($result);
+
+if ($numberOfRowsInQuestions > 0)
 {
 	$counter = 0;
 	while ($row = pg_fetch_row($result))
@@ -32,8 +45,27 @@ if ($numberOfRows > 0)
         	echo "</script>";
         	$counter++;
 	}
-}
+	$numberOfRows = $numberOfRowsInQuestions;
+} 
 
+else if ($numberOfRowsInCounting > 0)
+{
+	$counter = 0;
+	while ($row = pg_fetch_row($result))
+	{
+        	//fill php vars from db
+        	$questions = $row[0];
+        	$answers = $row[1];
+
+        	echo "<script language=\"javascript\">";
+
+        	echo "questions[$counter] = \"$questions\";";
+        	echo "answers[$counter] = \"$answers\";";
+        	echo "</script>";
+        	$counter++;
+	}
+	$numberOfRows = $numberOfRowsInCounting;
+}
 
 echo "<script language=\"javascript\">";
 echo "var numberOfRows = $numberOfRows;";
