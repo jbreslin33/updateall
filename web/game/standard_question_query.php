bbdcbcffe7bcc28cdcc0ef6defcdbc46e43677c6
jbreslin33 <jbreslin33@localhost>
2012-11-23 21:48:08 -0500

    trying to add math calcs

--- web/game/standard_question_query.php
+++ web/game/standard_question_query.php
@@ -1,37 +1,50 @@
 <?php
+
+echo "<script language=\"javascript\">";
+echo "var questions = new Array();";
+echo "var answers = new Array();";
+echo "</script>";
+
 //query the game table, eventually maybe there will be more than one result here which would be a choice of game for that level.
 $query = "select question, answer, question_order from questions where level_id = ";
 $query .= $_SESSION["next_level_id"];
 $query .= " ORDER BY question_order;";
-
 
 //get db result
 $result = pg_query($conn,$query) or die('Could not connect: ' . pg_last_error());
 
 //get numer of rows which will also be score needed
 $numberOfRows = pg_num_rows($result);
-$scoreNeeded = pg_num_rows($result);
+
+if ($numberOfRows > 0)
+{
+	$counter = 0;
+	while ($row = pg_fetch_row($result))
+	{
+        	//fill php vars from db
+        	$questions = $row[0];
+        	$answers = $row[1];
+
+        	echo "<script language=\"javascript\">";
+
+        	echo "questions[$counter] = \"$questions\";";
+        	echo "answers[$counter] = \"$answers\";";
+        	echo "</script>";
+        	$counter++;
+	}
+}
+
 
 echo "<script language=\"javascript\">";
 echo "var numberOfRows = $numberOfRows;";
 echo "var scoreNeeded = $numberOfRows;";
-echo "var questions = new Array();";
-echo "var answers = new Array();";
-
 echo "</script>";
 
-$counter = 0;
-while ($row = pg_fetch_row($result))
-{
-        //fill php vars from db
-        $questions = $row[0];
-        $answers = $row[1];
 
-        echo "<script language=\"javascript\">";
 
-        echo "questions[$counter] = \"$questions\";";
-        echo "answers[$counter] = \"$answers\";";
-        echo "</script>";
-        $counter++;
-}
+//could you just query counting then addition then multiplication then division etc using level and if there is something there use that?
+//answer: no
+
+
+
 ?>
