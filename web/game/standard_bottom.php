9cedbb4e0d0589378f252e801e36425eb9a48a9f
jbreslin33 <jbreslin33@localhost>
2012-12-08 16:26:25 -0500

    rid of application

--- web/game/standard_bottom.php
+++ web/game/standard_bottom.php
@@ -1,20 +1,20 @@
   	//RESET GAME TO START
-        game.resetGame();
+        GAME.resetGame();
 
         //START UPDATING
-        var t=setInterval("game.update()",32)
+        var t=setInterval("GAME.update()",32)
 
         //brian - update score in database
-        var t=setInterval("game.updateScore()",1000)
+        var t=setInterval("GAME.updateScore()",1000)
 				
 	//check if game has gone on too long
-        var t=setInterval("game.checkTime()",1000)
+        var t=setInterval("GAME.checkTime()",1000)
 }
 );
 
 window.onresize = function(event)
 {
-        mApplication.mWindow = window.getSize();
+        GAME.mWindow = window.getSize();
 }
 </script>
 
