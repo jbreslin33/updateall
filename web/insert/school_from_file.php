ecfd1566536f61ecfb6baf8199cb2515e4582af2
jbreslin33 <jbreslin33@localhost>
2012-12-12 15:16:34 -0500

    php to insert from file is working but not doing all that we want yet.

--- web/insert/school_from_file.php
+++ web/insert/school_from_file.php
@@ -15,35 +15,11 @@ include(getenv("DOCUMENT_ROOT") . "/src/database/insert_into_students.php");
 include(getenv("DOCUMENT_ROOT") . "/src/database/insert_first_level_transaction.php"); 
 
 //loop thru classes
-$number_of_classes = $_POST["number_of_classes"];
-
-for ($c = 0; $c < $number_of_classes; $c++)
-{
-
-	//************ class teacher ********************
-	//get a password
-	$password = getRandomPassword($conn);
-
-	//get a username
-	$newUsername = getNextUsernumber($conn);
-
-	//let's actually add the user
-	insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
-
-	//get new user id
-	$new_teacher_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
-
-	//insert student
-	insertIntoTeachers($conn,$new_teacher_id);
-
-	//insert student
-	insertIntoStudents($conn,$new_teacher_id,$new_teacher_id);
-
-	//insert first transaction for levels to lowest level
-	insertFirstLevelTransaction($conn,$new_teacher_id);
+//$number_of_classes = $_POST["number_of_classes"];
+$number_of_students = 10;
 
 	//now loop thru and add a class of students..
-	$number_of_students = $_POST["number_of_students"]; 
+	//$number_of_students = $_POST["number_of_students"]; 
 	for ($i = 0; $i < $number_of_students; $i++)
 	{
 		//************ class student ********************
@@ -51,7 +27,8 @@ for ($c = 0; $c < $number_of_classes; $c++)
 		$password = getRandomPassword($conn);
 
 		//get a username
-		$newUsername = getNextUsernumber($conn);
+		//$newUsername = getNextUsernumber($conn);
+		$newUsername = $i;
 
 		//let's actually add the user
 		insertIntoUsers($conn,$newUsername, $password, $_SESSION["school_id"]);
@@ -60,13 +37,12 @@ for ($c = 0; $c < $number_of_classes; $c++)
 		$new_student_id = selectUserID($conn, $_SESSION["school_id"],$newUsername,$password);
 
 		//insert student
-		insertIntoStudents($conn,$new_student_id,$new_teacher_id);
-
+		//insertIntoStudents($conn,$new_student_id,$new_teacher_id);
+		insertIntoStudents($conn,$new_student_id,1);
 
 		//insert first transaction for levels to lowest level
 		insertFirstLevelTransaction($conn,$new_student_id);
 	}
-}
 
 //go to success page
 header("Location: /web/select/student.php");
