ccca7192a06430af4f0da8f9b8c64087f6a5ba6b
jbreslin33 <jbreslin33@localhost>
2012-12-16 17:02:38 -0500

    now when you upload file it does inserts right away. how bout setting for updates????

--- web/insert/upload_file.php
+++ web/insert/upload_file.php
@@ -46,6 +46,8 @@ if ((($_FILES["file"]["type"] == "image/gif")
       move_uploaded_file($_FILES["file"]["tmp_name"],
       "upload/" . $_FILES["file"]["name"]);
       echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+	include(getenv("DOCUMENT_ROOT") . "/web/insert/class_bulk.php");
+
       }
     }
   }
