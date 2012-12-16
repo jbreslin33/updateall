833db1c20a0a576f18c3ba86b331b8ada982fb53
jbreslin33 <jbreslin33@localhost>
2012-12-16 13:57:26 -0500

    adding green links to upload

--- web/insert/links.php
+++ web/insert/links.php
@@ -4,5 +4,5 @@
 <li><a href="/web/insert/teacher.php">Insert Teachers</a></li>
 <li><a href="/web/insert/class_size.php">Insert Classes</a></li>
 <li><a href="/web/insert/school_size.php">Insert Schools</a></li>
-<li><a href="/web/insert/upload.html">Upload</a></li>
+<li><a href="/web/insert/upload.php">Upload</a></li>
 </ul>
