4185accd1980a9e18ef504a44464e8105b377aff
jbreslin33 <jbreslin33@localhost>
2012-12-16 17:51:28 -0500

    u

--- web/insert/upload_file.php
+++ web/insert/upload_file.php
@@ -37,17 +37,17 @@ if ((($_FILES["file"]["type"] == "image/gif")
     		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     		echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    		if (file_exists("upload/" . $_FILES["file"]["name"]))
-      		{
-      			echo $_FILES["file"]["name"] . " already exists. ";
-      		}
-    		else
-      		{
+    		//if (file_exists("upload/" . $_FILES["file"]["name"]))
+      		//{
+      		//	echo $_FILES["file"]["name"] . " already exists. ";
+      		//}
+    		//else
+      		//{
       			move_uploaded_file($_FILES["file"]["tmp_name"],
       			"upload/" . $_FILES["file"]["name"]);
       			echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
 			include(getenv("DOCUMENT_ROOT") . "/web/insert/class_bulk.php");
-      		}
+      		//}
     	}
 }
 else
